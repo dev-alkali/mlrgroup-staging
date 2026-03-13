5a2388faf1eeeba9cc3857a1093e541fb700821d
devteam <development@alkalidesigns.com>
2026-03-13 19:26:05 +0530

    Update two-column.php

--- inc/integrations/acf-blocks/two-column/two-column.php
+++ inc/integrations/acf-blocks/two-column/two-column.php
@@ -23,7 +23,7 @@ $description = get_sub_field('description');
 
 ?>
     <section id="<?php echo esc_attr($id); ?>" class="<?php echo esc_attr($className); ?> w-full px-4 min-[600px]:px-10 pt-[118px] pb-[120px]">
-      <div class="gap-10 w-full max-w-[1920px] mx-auto flex ">
+      <div class="gap-10 w-full max-w-[1920px] mx-auto flex items-center flex-wrap">
         <div class="w-[50%]">
           <figure><?php echo wp_get_attachment_image(get_sub_field('image'), 'full', false, ['class' => 'w-full h-auto']); ?></figure>
         </div>
