80069a59e566ddc5ddf8f1d95b356a5d52edd545
devteam <development@alkalidesigns.com>
2026-03-16 14:47:41 +0530

    Update trusted-brand.php

--- inc/integrations/acf-blocks/trusted-brand/trusted-brand.php
+++ inc/integrations/acf-blocks/trusted-brand/trusted-brand.php
@@ -17,15 +17,14 @@ if (!empty($block['className'])) {
 }
 ?>
 <?php if (have_rows('trusted_brand')) :  while (have_rows('trusted_brand')) : the_row(); ?>
-    <section id="<?php echo esc_attr($id); ?>" class="<?php echo esc_attr($className); ?> flex flex-col w-full items-center p-0 min-[890px]:pt-5 min-[890px]:pb-0  min-[890px]:px-5 bg-white">
+    <section id="<?php echo esc_attr($id); ?>" class="<?php echo esc_attr($className); ?> flex flex-col w-full items-center min-[890px]:pt-10 min-[890px]:pb-10  min-[890px]:px-5 bg-white">
       <?php if (have_rows('brands')) :  while (have_rows('brands')) : the_row(); ?>
-          <section class=" flex items-center gap-2 pl-4 pr-4 min-[890px]:pl-[58px] min-[890px]:pr-0 py-10  w-full max-w-[1920px] bg-white">
+          <section class="flex items-center gap-2 pl-4 pr-4 py-10 w-full max-w-[1920px] bg-white">
             <div class="relative w-full ">
               <div class="flex items-start min-[600px]:justify-start justify-center gap-2 min-[600px]:gap-3 shrink-0">
                 <div class="relative mt-[1.5px] min-[600px]:mt-[3px]"><img class="w-4 min-[600px]:w-5 " src="<?= get_template_directory_uri() ?>/assets/imgs/Arrow-blue-brands.svg" alt="arrow"></div>
                 <p class="text-[16px] font-medium min-[600px]:text-2xl min-[600px]:tracking-[-2%] leading-6 min-[600px]:leading-8 text-black font-heading"><?= wp_kses_post(get_sub_field('title')) ?></p>
               </div>
-
               <!-- MARQUEE -->
               <div class="flex-1  overflow-hidden relative h-[91.15px] md:h-auto">
                 <div class="absolute -top-4 left-[-7%] w-[133px] max-[890px]:hidden h-[139px] bg-white z-20 blur-[16px]"></div>
