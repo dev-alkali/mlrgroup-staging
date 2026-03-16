1254d141add4d92d5d725697ffa34e08a8dde025
devteam <development@alkalidesigns.com>
2026-03-16 16:52:52 +0530

    Update acf.php

--- inc/integrations/acf.php
+++ inc/integrations/acf.php
@@ -3,10 +3,6 @@
 add_action('init', 'register_acf_blocks');
 function register_acf_blocks()
 {
-  register_block_type(__DIR__ . '/acf-blocks/client_logos');
-  register_block_type(__DIR__ . '/acf-blocks/counter-section');
-  register_block_type(__DIR__ . '/acf-blocks/map');
-  
   register_block_type(__DIR__ . '/acf-blocks/hero');
   register_block_type(__DIR__ . '/acf-blocks/services');
   register_block_type(__DIR__ . '/acf-blocks/services-solutions');
@@ -16,8 +12,10 @@ function register_acf_blocks()
   register_block_type(__DIR__ . '/acf-blocks/our-work-grid');
   register_block_type(__DIR__ . '/acf-blocks/inner-hero');
   register_block_type(__DIR__ . '/acf-blocks/two-column');
-  register_block_type(__DIR__ . '/acf-blocks/trusted-brand');
-  
+  //register_block_type(__DIR__ . '/acf-blocks/trusted-brand');
+  register_block_type(__DIR__ . '/acf-blocks/client_logos');
+  register_block_type(__DIR__ . '/acf-blocks/counter-section');
+  register_block_type(__DIR__ . '/acf-blocks/map');
   
   
   
