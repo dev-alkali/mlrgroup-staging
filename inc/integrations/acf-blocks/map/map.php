e83ebc7bc829162a29b110cd98ed7d1288cba2c3
devteam <development@alkalidesigns.com>
2026-03-16 17:47:20 +0530

    Update map.php

--- inc/integrations/acf-blocks/map/map.php
+++ inc/integrations/acf-blocks/map/map.php
@@ -15,8 +15,16 @@ if (!empty($block['className'])) {
 }
 ?>
 
-<?php if (have_rows('map')) :  while (have_rows('map')) : the_row(); ?>
-
-
-<?php endwhile;
-endif; ?>
+<?php if (have_rows('map-section')) :  while (have_rows('map-section')) : the_row(); ?>
+<section>
+  <div class="container">
+    <div class="map__content">
+      <h2 class="map__title"><?php echo get_sub_field('map_title'); ?></h2>
+      <div class="map__address"><?php echo get_sub_field('map_address'); ?></div>
+    </div>
+    <div class="map__iframe">
+      <?php echo get_sub_field('map_iframe'); ?>
+    </div>
+  </div>
+</section>
+<?php endwhile; endif; ?>
