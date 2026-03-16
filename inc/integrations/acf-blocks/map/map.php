9d8fd0a66f326c6fb883b26835ce65298de7d8f8
devteam <development@alkalidesigns.com>
2026-03-16 16:58:06 +0530

    Enable client logos block; clean up map template
    Uncomment the registration for the client_logos ACF block so it is available in the editor. Remove stray placeholder/test HTML from inc/integrations/acf-blocks/map/map.php, leaving the ACF loop intact to avoid rendering unintended content.

--- inc/integrations/acf-blocks/map/map.php
+++ inc/integrations/acf-blocks/map/map.php
@@ -17,9 +17,6 @@ if (!empty($block['className'])) {
 
 <?php if (have_rows('map')) :  while (have_rows('map')) : the_row(); ?>
 
-<section id="<?php echo esc_attr($id); ?>" class="<?php echo esc_attr($className . ' ' . $bg_class); ?> <?php echo esc_attr($overlay_class); ?> flex w-full justify-center px-4 min-[600px]:px-10 py-[60px] min-[600px]:py-[120px]">      
-      Testing
-      
-</section>
+
 <?php endwhile;
 endif; ?>
