440915f61268ee7a04a51bbf1d971947fb687404
devteam <development@alkalidesigns.com>
2026-03-18 19:37:10 +0530

    Update client-logos.php

--- inc/integrations/acf-blocks/client-logos/client-logos.php
+++ inc/integrations/acf-blocks/client-logos/client-logos.php
@@ -62,7 +62,7 @@ if (!empty($block['className'])) {
 
               <?php if ($show_filter && ! empty($industries)) : ?>
                   <div class="logo-filter mb-6">
-                      <select id="industry-filter" class="border rounded px-4 py-2">
+                      <span>Filter by</span> <select id="industry-filter" class="border rounded px-4 py-2">
                           <option value="all">All Industries</option>
                           <?php foreach ($industries as $industry) : ?>
                               <option value="<?php echo esc_attr($industry); ?>">
