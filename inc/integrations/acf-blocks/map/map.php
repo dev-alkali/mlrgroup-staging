395c364d938d30fa5ffc278c0d493e830cd4ee0e
devteam <development@alkalidesigns.com>
2026-03-18 18:49:22 +0530

    Update map.php

--- inc/integrations/acf-blocks/map/map.php
+++ inc/integrations/acf-blocks/map/map.php
@@ -21,11 +21,11 @@ $title2 = get_sub_field('title_row_2');
 $map_code = get_sub_field('map_code');
 
 ?>
-<section id="<?php echo esc_attr($id); ?>" class="<?php echo esc_attr($className . ' ' . $bg_class); ?> <?php echo esc_attr($overlay_class); ?> px-4 min-[600px]:px-10 py-[60px] min-[600px]:py-[120px]">      
+<section id="<?php echo esc_attr($id); ?>" class="<?php echo esc_attr($className . ' ' . $bg_class); ?> <?php echo esc_attr($overlay_class); ?> map-sec px-4 md:px-10 py-[60px] md:py-[120px]">
   <div class="wrapper">
       <?php if ($title1 || $title2) : ?>
-        <div class="serve-heading">
-          <h2 class="text-[44px] flex flex-col min-[600px]:text-[54px] min-[767px]:text-[68px] tracking-[-2%] leading-[56px] min-[600px]:leading-[64px] min-[767px]:leading-[78px] font-heading">
+        <div class="serve-heading md:mb-[80px] md-[32px]">
+          <h2 class="flex flex-col font-heading font-bold text-[clamp(36px,4.5vw,68px)] leading-[clamp(44px,5.2vw,78px)] tracking-[-0.02em]">
             <span class="font-bold text-neutral-800"><?= wp_kses_post($title1) ?></span>
             <span class="font-light text-neutral-500"><?= wp_kses_post($title2) ?></span>
           </h2>
