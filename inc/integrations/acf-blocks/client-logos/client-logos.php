089d8cd9af69583b13f837ca030ee67198169502
devteam <development@alkalidesigns.com>
2026-03-19 12:32:31 +0530

    Add view-more arrow rotation styles
    Introduce hover animation for the CTA arrow and scope it with a new wrapper class. Adds CSS to animate .view-more-btn .arrow (rotation with transition) and applies .view-more-btn to the client-logos block wrapper so the arrow rotates 45° on hover. Also includes minor newline/whitespace fixes in the CSS file.

--- inc/integrations/acf-blocks/client-logos/client-logos.php
+++ inc/integrations/acf-blocks/client-logos/client-logos.php
@@ -101,7 +101,7 @@ if (!empty($block['className'])) {
                   $link_title = $link['title'];
                   $link_target = $link['target'] ? $link['target'] : '_self';
                   ?>
-                  <div class="text-center mt-[32px] md:mt-[40px]">
+                  <div class="text-center mt-[32px] md:mt-[40px] view-more-btn">
                   <a class="inline-flex  gap-2 relative " href="<?php echo esc_url( $link_url ); ?>" target="<?php echo esc_attr( $link_target ); ?>">
                       <span class="font-bold text-accent text-lg leading-7 uppercase relative w-fit font-heading tracking-[0]"><?php echo esc_html( $link_title ); ?></span>
                       <img decoding="async" class="arrow relative w-4 h-4 mt-1" src="https://wordpress-755960-6249701.cloudwaysapps.com/wp-content/themes/Mlrgroup/assets/imgs/Arrow-red.svg">
