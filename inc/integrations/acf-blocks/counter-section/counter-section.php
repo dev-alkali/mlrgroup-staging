04788669c60680fe3818ae0353e8238278979104
devteam <development@alkalidesigns.com>
2026-03-19 17:00:44 +0530

    Update counter-section.php

--- inc/integrations/acf-blocks/counter-section/counter-section.php
+++ inc/integrations/acf-blocks/counter-section/counter-section.php
@@ -22,7 +22,7 @@ if (!empty($block['className'])) {
     <section id="<?php echo esc_attr($id); ?>" class="<?php echo esc_attr($className); ?> performance flex justify-center  px-4 md:px-10 pb-[60px] md:pb-[120px]">
       <div class="w-full wrapper flex items-center justify-between md:justify-evenly lg:justify-between gap-8 md:gap-4 max-[1440px]:flex-wrap flex-1">
             <?php if (have_rows('counter_list')) :  while (have_rows('counter_list')) : the_row(); ?>
-              <div class="performance-item inline-flex flex-col md:max-w-[284px] w-full w-max md:items-center gap-1 md:gap-[25px]">
+              <div class="performance-item inline-flex flex-col w-full md:items-start lg:items-center md:max-w-full lg:max-w-[284px] gap-1 md:gap-[25px]">
                   <div class="relative flex items-center justify-start sm:justify-center text-[clamp(40px,5vw,64px)] leading-[clamp(48px,5.5vw,72px)] font-bold tracking-[-0.02em] font-[Poppins]">
                       <div class="invisible text-[#262626]" aria-hidden="true"><?= wp_kses_post(get_sub_field('value')) ?></div>
                       <div class="absolute count-box tabular-nums text-[#262626]"><?= wp_kses_post(get_sub_field('value')) ?></div>
