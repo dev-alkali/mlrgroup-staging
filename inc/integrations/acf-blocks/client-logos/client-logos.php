71f0f9fc0449f1beb51a6984fc55b109d740a407
devteam <development@alkalidesigns.com>
2026-03-19 12:50:26 +0530

    Add logo filter styles and adjust filter markup
    Add .logo-filter CSS and style the filter select with a custom arrow and reset native appearances. Update the client-logos block markup to add spacing to the "Filter by" label (mr-[12px]) and remove the previous utility classes from the select so it uses the new CSS. Note: the select rule contains a "pading" typo that should be corrected to "padding".

--- inc/integrations/acf-blocks/client-logos/client-logos.php
+++ inc/integrations/acf-blocks/client-logos/client-logos.php
@@ -61,7 +61,7 @@ if (!empty($block['className'])) {
 
               <?php if ($show_filter && ! empty($industries)) : ?>
                   <div class="logo-filter mb-6 w-full text-right">
-                      <span>Filter by</span> <select id="industry-filter" class="border rounded px-4 py-2">
+                      <span class="mr-[12px]">Filter by</span> <select id="industry-filter" class="">
                           <option value="all">All Industries</option>
                           <?php foreach ($industries as $industry) : ?>
                               <option value="<?php echo esc_attr($industry); ?>">
