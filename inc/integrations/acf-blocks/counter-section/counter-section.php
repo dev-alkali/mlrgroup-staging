f7b1a984f930006ac754a3543a6d268dbfc449b0
devteam <development@alkalidesigns.com>
2026-03-19 14:53:23 +0530

    Update counter-section.php

--- inc/integrations/acf-blocks/counter-section/counter-section.php
+++ inc/integrations/acf-blocks/counter-section/counter-section.php
@@ -21,7 +21,7 @@ if (!empty($block['className'])) {
 
     <section id="<?php echo esc_attr($id); ?>" class="<?php echo esc_attr($className); ?>  flex justify-center  px-4 md:px-10 py-[60px] md:py-[120px]">
       <div class="w-full wrapper">
-        <div class="flex items-center max-w-[938px] w-full justify-between min-[567px]:justify-evenly min-[1023px]:justify-between gap-8 min-[600px]:gap-4 max-[1440px]:flex-wrap flex-1">
+        <div class="flex items-center justify-between md:justify-evenly lg:justify-between gap-8 md:gap-4 max-[1440px]:flex-wrap flex-1">
             <?php if (have_rows('counter_list')) :  while (have_rows('counter_list')) : the_row(); ?>
                     <div class="performance-item inline-flex flex-col max-[567px]:max-w-[163px] max-[567px]:w-full  w-max min-[567px]:items-center gap-1 min-[600px]:gap-3">
                         <div class="relative flex items-center justify-items-start min-[567px]:justify-center text-[40px] min-[600px]:text-[50px] min-[890px]:text-6xl leading-[48px] min-[600px]:leading-[52px] font-bold tracking-[-2%] font-[poppins]">
