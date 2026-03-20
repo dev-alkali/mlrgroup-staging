e5ebe74426b9d14930f28fb72f20448d0a1e8001
devteam <development@alkalidesigns.com>
2026-03-20 17:04:36 +0530

    Update performance.php

--- inc/integrations/acf-blocks/performance/performance.php
+++ inc/integrations/acf-blocks/performance/performance.php
@@ -18,52 +18,27 @@ if (!empty($block['className'])) {
 
         <section id="<?php echo esc_attr($id); ?>" class="performance <?php echo esc_attr($className); ?> flex flex-col w-full items-center gap-[60px] pt-10 pb-[76px] md:pb-[180px] px-4 md:px-10 bg-black overflow-hidden">
             <div class="flex flex-col items-center gap-5 w-full max-w-[1920px] mb-[-76px] md:mb-[-180px]">
-
-               
-
-                <div class="flex max-w-[1920px] flex-col lg:flex-row w-full lg:items-center gap-8 sm:gap-12 lg:gap-24 bg-black">
-
-    <!-- Title -->
-    <div class="flex max-w-[322px] items-center justify-center gap-2">
-        <h2 class="text-[44px] md:text-5xl leading-[56px] font-bold tracking-[-2%] font-[poppins] text-white">
-            <?= wp_kses_post(get_sub_field('title')) ?>
-            <span class="text-[#4a78ff]">.</span>
-        </h2>
-    </div>    
-
-    <!-- Grid Items -->
-    
-    <div class="grid w-full max-w-[938px] gap-8 md:gap-6 grid-cols-2 xl:grid-cols-4 flex-1">
-        
-        <?php if (have_rows('infos')) : while (have_rows('infos')) : the_row(); ?>
-
-            <div class="performance-item flex flex-col items-center gap-2 md:gap-3 text-center">
                 
-                <!-- Number -->
-                <div class="relative flex items-center justify-center text-[40px] md:text-[50px] lg:text-6xl leading-[48px] md:leading-[52px] font-bold tracking-[-2%] font-[poppins] text-white">
-
-                    <div class="invisible" aria-hidden="true">
-                        <?= wp_kses_post(get_sub_field('value')) ?>
-                    </div>
-                    <div class="absolute count-box tabular-nums">
-                        <?= wp_kses_post(get_sub_field('value')) ?>
+                <div class="flex max-w-[1920px] justify-between flex-col min-[800px]:flex-row w-full min-[800px]:items-center gap-8 min-[600px]:gap-[50px] min-[890px]:gap-[100px] bg-black">
+                    <div class="flex max-[1441px]:max-w-[322px] items-center md::justify-center gap-2">
+                        <h2 class="text-[44px] md::text-5xl leading-[56px] font-bold  tracking-[-2%] font-[poppins] text-white">
+                            <?= wp_kses_post(get_sub_field('title')) ?>
+                            <span class="text-[#4a78ff]">.</span>
+                        </h2>
+                    </div>    
+                    <div class="flex items-center max-w-[938px] w-full justify-between md:justify-evenly lg:justify-between gap-8 md:gap-4 max-[1440px]:flex-wrap flex-1">
+                        <?php if (have_rows('infos')) :  while (have_rows('infos')) : the_row(); ?>
+                                <div class="performance-item inline-flex flex-col md:max-w-[163px] md:w-full  w-max md:items-center gap-1 md:gap-3">
+                                    <div class="relative flex items-center justify-items-start md:justify-center text-[40px] md:text-[50px] min-[890px]:text-6xl leading-[48px] md:leading-[52px] font-bold tracking-[-2%] font-[poppins] text-white">
+                                        <div class="invisible" aria-hidden="true"><?= wp_kses_post(get_sub_field('value')) ?></div>
+                                        <div class="absolute count-box tabular-nums"><?= wp_kses_post(get_sub_field('value')) ?></div>
+                                    </div>
+                                    <p class="md:max-w-[133px] text-[14px] md:text-base leading-[20px] md:leading-6 text-white font-body md:text-center"><?= wp_kses_post(get_sub_field('description')) ?></p>
+                                </div>
+                        <?php endwhile;
+                        endif; ?>
                     </div>
                 </div>
-
-                <!-- Description -->
-                <p class="text-[14px] md:text-base leading-[20px] md:leading-6 text-white font-body">
-                    <?= wp_kses_post(get_sub_field('description')) ?>
-                </p>
-
-            </div>
-
-        <?php endwhile; endif; ?>
-
-    </div>
-
-</div>
-
-
 
                 <div class="arrows-row flex max-w-[1360px] w-full items-center justify-center gap-3  overflow-hidden max-[1200px]:[&>img:nth-child(1)]:hidden max-[1024px]:[&>img:nth-child(2)]:hidden max-[768px]:[&>img:nth-child(3)]:hidden max-[640px]:[&>img:nth-child(4)]:hidden">
                     <img src="<?= get_template_directory_uri() ?>/assets/imgs/Arrow-thin.svg" class="arrow w-[clamp(49px,8vw,120px)] h-[clamp(47px,8vw,117px)]" alt="">
