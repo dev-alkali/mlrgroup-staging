1d2ee52986ce0b5e99d5ff03d9982b50cb66ef46
devteam <development@alkalidesigns.com>
2026-03-20 20:18:55 +0530

    Update award-winning-logo.php

--- inc/integrations/acf-blocks/award-winning-logo/award-winning-logo.php
+++ inc/integrations/acf-blocks/award-winning-logo/award-winning-logo.php
@@ -33,8 +33,11 @@ if (!empty($block['className'])) {
 
       <!-- MARQUEE -->
       <div class="flex-1 overflow-hidden relative h-[131px] md:h-auto">
-        <div class="absolute -top-4 left-[-7%] w-[189px] lg:hidden h-[200px] bg-white z-20 blur-[16px]"></div>
-        <div class="absolute -top-4 right-[-5%] w-[189px] h-[200px] lg:hidden bg-white z-20  blur-[16px]"></div>
+
+        <div class="absolute -top-4 left-[-7%] w-[189px] max-[890px]:hidden h-[200px] bg-white z-20 blur-[16px]"></div>
+        <div class="absolute -top-4 right-[-5%] w-[189px] h-[200px] max-[890px]:hidden bg-white z-20  blur-[16px]"></div>
+
+
         <div class="marquee-wrapper overflow-hidden w-full">
           <div class="marquee-track relative">
             <div class="marquee-group">
