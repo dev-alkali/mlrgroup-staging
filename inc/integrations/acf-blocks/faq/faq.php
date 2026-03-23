5071a4a7e804709a62336657831c133813c4d3c9
nirav-e2m <development@alkalidesigns.com>
2026-03-23 19:22:17 +0530

    Update faq.php

--- inc/integrations/acf-blocks/faq/faq.php
+++ inc/integrations/acf-blocks/faq/faq.php
@@ -60,11 +60,13 @@ $mobileFlex = 'flex-col-reverse';
 				<div class="">
 					<?php if($faq_lists): ?>
 						<div class="flex flex-col gap-[20px]">
-							<?php foreach($faq_lists as $faq): ?>
+							<?php 
+							 $i = 1;
+							foreach($faq_lists as $faq $i++): ?>
 								<div class="flex flex-col bg-black text-white">
-									<h3 class="font-heading question font-bold text-white text-[clamp(18px,2.2vw,20px)] leading-[clamp(26px,2.6vw,28px)]"><?php echo $faq['question']; ?></h3>
-									<div class="answer">
-										<div class="font-body font-normal text-white text-[clamp(16px,1.8vw,18px)] leading-[clamp(24px,2.8vw,26px)]"><?php echo $faq['answer']; ?></div>
+									<h3 class="font-heading question font-bold text-white text-[clamp(18px,2.2vw,20px)] leading-[clamp(26px,2.6vw,28px)] max-w-[90%]"><?php echo $i; ?> <?php echo $faq['question']; ?></h3>
+									<div class="answer max-w-[90%]">
+										<div class="font-body font-normal text-white text-[clamp(14px,1.7vw,16px)] leading-[clamp(24px,2.8vw,26px)] "><?php echo $faq['answer']; ?></div>
 									</div>
 								</div>
 							<?php endforeach; ?>
