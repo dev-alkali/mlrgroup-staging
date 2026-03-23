11892da19cd94bac894645ce21e878423583f5e2
devteam <development@alkalidesigns.com>
2026-03-23 18:47:58 +0530

    Update case-studies.php

--- inc/integrations/acf-blocks/case-studies/case-studies.php
+++ inc/integrations/acf-blocks/case-studies/case-studies.php
@@ -1,69 +1,129 @@
 <?php
 
 /**
- * Case Studies Block Template.
+ * Collection Block Template.
  */
 
-$id = 'case-studies' . $block['id'];
+$id = 'collection-' . $block['id'];
 if (!empty($block['anchor'])) {
   $id = $block['anchor'];
 }
 
-$className = 'case-studies';
+$className = 'collection';
 if (!empty($block['className'])) {
   $className .= ' ' . $block['className'];
 }
-
 ?>
-<?php if (have_rows('case-studies')) :  while (have_rows('case-studies')) : the_row();
+<?php if (have_rows('case_studies')) :  while (have_rows('case_studies')) : the_row(); ?>
 
-$title_row_1 = get_sub_field('title_row_1');
-$title_row_2 = get_sub_field('title_row_2');
-$description = get_sub_field('description');
-//$left_image_or_right_image = get_sub_field('left_image_or_right_image');
-$layout = get_sub_field('left_image_or_right_image'); // left / right
-// Desktop layout
-$desktopFlex = ($layout === 'Right Image') ? 'md:flex-row-reverse' : 'md:flex-row';
+    <section id="<?php echo esc_attr($id); ?>" class="<?php echo esc_attr($className); ?> flex justify-center px-4 min-[600px]:px-10 py-[60px] min-[600px]:py-[120px] bg-white">
+      <div class="flex flex-col w-full items-start gap-8 min-[600px]:gap-[60px] max-w-[1920px]">
+        <?php if (have_rows('header_content')) :  while (have_rows('header_content')) : the_row(); ?>
 
-// Mobile: always content first, image bottom
-$mobileFlex = 'flex-col-reverse';
+            <div class="flex flex-col  items-start gap-5 relative ">
+              <h2
+                class=" flex flex-col font-heading text-[44px] min-[600px]:text-[54px] min-[767px]:text-[68px] tracking-[-2%] max-w-[700px] leading-[56px] min-[600px]:leading-[64px] min-[767px]:leading-[78px]">
+                <span class="font-bold text-neutral-800 "><?= wp_kses_post(get_sub_field('title_row_1')) ?> </span>
+                <span class="font-light text-neutral-500 "><?= wp_kses_post(get_sub_field('title_row_2')) ?></span>
+              </h2>
 
-?>
-    <section id="<?php echo esc_attr($id); ?>" class="<?php echo esc_attr($className); ?> two-col-sec px-4 md:px-10 py-[60px] md:py-[120px]">
-      <!-- <div class="gap-[30px] md:gap-[60px] w-full wrapper flex flex-col md:flex-row items-center flex-wrap"> -->
-      <div class="gap-[30px] md:gap-[60px] w-full wrapper flex flex-col <?php echo $mobileFlex . ' ' . $desktopFlex; ?> items-center">
 
-        <div class="w-full flex-1">
-          <figure><?php echo wp_get_attachment_image(get_sub_field('image'), 'full', false, ['class' => 'w-full h-auto']); ?></figure>
-        </div>
+              <p
+                class=" font-body  text-neutral-600 text-xl tracking-[0] leading-7">
+                <?= wp_kses_post(get_sub_field('paragraph')) ?>
+              </p>
 
-        <div class="w-full flex-1">
-          <div class="">
-            <?php if($title_row_1 || $title_row_2): ?>
-            <h2 class="text-[clamp(32px,6vw,68px)] leading-[clamp(40px,7vw,76px)] tracking-[-4%] text-[#262626] font-heading mb-[20px]">
-              <?php if($title_row_1): ?>
-                  <span class="font-bold"><?= wp_kses_post($title_row_1) ?></span>
-              <?php endif; ?>
-              <?php if($title_row_2): ?>
-                  <span class="font-light text-[#737373]"><?= wp_kses_post($title_row_2) ?></span>
-              <?php endif; ?>
-            </h2>
-            <?php endif; ?>
-            <?php if($description): ?>
-              <div class="w-full text-[clamp(16px,3vw,18px)] leading-[28px] text-[#525252] font-body flex flex-col gap-[30px]"><?= wp_kses_post($description) ?></div>
-            <?php endif; ?>
+            </div>
+        <?php endwhile;
+        endif; ?>
 
-            <?php 
-              $link = get_sub_field('button');
-              if( $link ): 
-                  $link_url = $link['url'];
-                  $link_title = $link['title'];
-                  $link_target = $link['target'] ? $link['target'] : '_self';
+        <?php if (have_rows('main_content')) :  while (have_rows('main_content')) : the_row(); ?>
+            <div class="flex-col items-start max-[600px]:gap-6 max-[1024px]:gap-10 w-full flex-[0_0_auto] flex relative">
+              <?php if (have_rows('collection_rows')) :  while (have_rows('collection_rows')) : the_row(); ?>
+                  <?php
                   ?>
-                  <a class="btn-primary mt-[40px]" href="<?php echo esc_url( $link_url ); ?>" target="<?php echo esc_attr( $link_target ); ?>"><?php echo esc_html( $link_title ); ?></a>
-              <?php endif; ?>
-          </div>
-        </div>
+                  <?php
+                  $row_height = "";
+                  if (wp_kses_post(get_sub_field('row_height')) == 'normal') {
+                    // 1440 (largura total) / 409 (altura desejada)
+                    $row_height = ' min-[1441px]:aspect-[1440/409] max-[1441px]:h-[409px] max-[1024px]:h-auto ';
+                  } elseif (wp_kses_post(get_sub_field('row_height')) == 'big') {
+                    // 1440 (largura total) / 434 (altura desejada)
+                    $row_height = ' min-[1441px]:aspect-[1440/434] max-[1441px]:h-[434.56px] max-[1024px]:h-auto ';
+                  }
+                  ?>
+                  <div class="flex flex-col min-[1024px]:flex-row max-[600px]:gap-6 max-[1024px]:gap-10 items-start relative <?= $row_height ?> w-full collection-parent">
+                    <?php if (have_rows('items')) :  while (have_rows('items')) : the_row(); ?>
+
+                        <?php
+                        $bg_color = '';
+                        $bg_image = esc_url(get_sub_field('bg_image'));
+                        if (wp_kses_post(get_sub_field('bg_color')) == 'white') {
+                          $bg_color = "bg-white";
+                        } elseif (wp_kses_post(get_sub_field('bg_color')) == 'gray') {
+                          $bg_color = "bg-[#CCCCCC]";
+                        } elseif (wp_kses_post(get_sub_field('bg_color')) == 'blue') {
+
+                          $bg_color = "bg-[#4a78ff]";
+                        }
+                        $text_color = "";
+                        $arrow_color = "";
+                        if (wp_kses_post(get_sub_field('text_color')) == 'white') {
+                          $text_color = "text-white";
+                          $arrow_color = get_template_directory_uri() . "/assets/imgs/Arrow.svg";
+                        } elseif (wp_kses_post(get_sub_field('text_color')) == 'black') {
+                          $text_color = "text-black";
+                          $arrow_color = get_template_directory_uri() . "/assets/imgs/Arrow-black.svg";
+                        }
+
+                        ?>
+                        <div class="<?= $text_color ?> collection-item h-[380px] min-[600px]:h-[420px] max-[1024px]:w-full min-[1024px]:h-full  min-[1024px]:flex-1 min-[1024px]:grow <?= $bg_image == "" ? $bg_color : "" ?>" <?= $bg_image !== "" ? 'style="
+    background-image: url(' . $bg_image . ');
+    background-position: 50% 50%;
+    background-size: cover;
+    background-repeat: no-repeat;
+  "' : ""; ?>>
+                          <a href="<?= wp_kses_post(get_sub_field('link_path')) ?>" class=" <?= $bg_image == "" ? "color-bg-hover" : "collection-gradient-box" ?> flex flex-col  items-end justify-between w-full h-full px-6 py-10 relative">
+                            <div class="flex flex-col relative z-20 items-start <?= esc_url(get_sub_field('icon')) !== "" ? "" : "pt-[88px]"; ?> gap-5 relative self-stretch w-full flex-[0_0_auto]">
+                              <?php if (esc_url(get_sub_field('icon')) !== ""): ?>
+                                <img
+                                  Crop
+                                  class="relative w-[70px] h-[70px] object-cover"
+                                  src=" <?= esc_url(get_sub_field('icon')) ?>"
+                                  alt="icon" />
+                              <?php endif; ?>
+
+                              <p
+                                class="relative  font-heading text-[20px] min-[600px]:text-[24px] max-w-[406px] tracking-[-2%] leading-7 min-[600px]:leading-8">
+                                <?= wp_kses_post(get_sub_field('title')) ?>
+                              </p>
+                            </div>
+
+                            <p class="inline-flex  z-20  gap-2 relative flex-[0_0_auto]">
+                              <span
+                                class="relative w-fit  uppercase font-heading font-semibold text-base text-center tracking-[0] leading-6 min-[600px]:leading-[18px] whitespace-nowrap">
+                                <?= wp_kses_post(get_sub_field('link_label')) ?>
+                              </span>
+                              <img class="relative w-4 h-4 max-[600px]:mt-[3px] arrow" src="<?= $arrow_color ?>" alt="arrow" />
+
+                            </p>
+                          </a>
+
+                        </div>
+                    <?php endwhile;
+                    endif; ?>
+
+
+
+
+                  </div>
+              <?php endwhile;
+              endif; ?>
+
+            </div>
+
+        <?php endwhile;
+        endif; ?>
       </div>
     </section>
 <?php endwhile;
