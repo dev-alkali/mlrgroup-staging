da1c1739ca1d345376c5b5df5de15dfb30375490
devteam <development@alkalidesigns.com>
2026-03-23 15:15:46 +0530

    Update services-solutions.php

--- inc/integrations/acf-blocks/services-solutions/services-solutions.php
+++ inc/integrations/acf-blocks/services-solutions/services-solutions.php
@@ -25,7 +25,7 @@ if (!empty($block['className'])) {
                             ?>
 
                             <?php if (!empty($title_bold) || !empty($title_light)) : ?>
-                            <h2 class="solutions-header__title text-[44px] md:text-5xl leading-[56px] md:leading-[60px] tracking-[-2%] font-heading text-neutral-800">
+                            <h2 class="solutions-header__title text-[44px] md:text-5xl leading-[56px] md:leading-[60px] tracking-[-2%] font-heading text-neutral-800 md:min-w-[515px]">
                                 <span class="font-bold text-neutral-800"><?= wp_kses_post(get_sub_field('title_bold')) ?></span>
                                 <span class="font-light text-neutral-500"><?= wp_kses_post(get_sub_field('title_light')) ?><span class="text-accent font-bold">.</span></span>
                             </h2>                                
