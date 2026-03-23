82b2174a6550c82681efe5b40ca6e2749f45fe31
devteam <development@alkalidesigns.com>
2026-03-23 12:34:57 +0530

    Update partnership.php

--- inc/integrations/acf-blocks/partnership/partnership.php
+++ inc/integrations/acf-blocks/partnership/partnership.php
@@ -31,15 +31,9 @@ $description = get_sub_field('description');
 
 ?>
     <section id="<?php echo esc_attr($id); ?>" class="<?php echo esc_attr($className); ?> two-col-sec px-4 md:px-10 py-[60px] md:py-[120px]">
-      <!-- <div class="gap-[30px] md:gap-[60px] w-full wrapper flex flex-col md:flex-row items-center flex-wrap"> -->
-      <div class="gap-[30px] md:gap-[60px] w-full wrapper flex flex-col <?php echo $mobileFlex . ' ' . $desktopFlex; ?> items-center">
-
-        <div class="w-full flex-1">
-          <figure><?php echo wp_get_attachment_image(get_sub_field('image'), 'full', false, ['class' => 'w-full h-auto']); ?></figure>
-        </div>
-
-        <div class="w-full flex-1">
-          <div class="">
+      <div class="wrapper">
+        <div class="top-title-sec">
+          <div class="left-title">
             <?php if($title_row_1 || $title_row_2): ?>
             <h2 class="text-[clamp(32px,6vw,68px)] leading-[clamp(40px,7vw,76px)] tracking-[-4%] text-[#262626] font-heading mb-[20px]">
               <?php if($title_row_1): ?>
@@ -50,22 +44,23 @@ $description = get_sub_field('description');
               <?php endif; ?>
             </h2>
             <?php endif; ?>
-            <?php if($description): ?>
-              <div class="w-full text-[clamp(16px,3vw,18px)] leading-[28px] text-[#525252] font-body flex flex-col gap-[30px]"><?= wp_kses_post($description) ?></div>
-            <?php endif; ?>
+          </div>
+          <?php if($description): ?>
+            <div class="w-full text-[clamp(16px,3vw,18px)] leading-[28px] text-[#525252] font-body flex flex-col gap-[30px]"><?= wp_kses_post($description) ?></div>
+          <?php endif; ?>
+        </div>
 
-            <?php 
-              $link = get_sub_field('button');
-              if( $link ): 
-                  $link_url = $link['url'];
-                  $link_title = $link['title'];
-                  $link_target = $link['target'] ? $link['target'] : '_self';
-                  ?>
-                  <a class="btn-primary mt-[40px]" href="<?php echo esc_url( $link_url ); ?>" target="<?php echo esc_attr( $link_target ); ?>"><?php echo esc_html( $link_title ); ?></a>
-              <?php endif; ?>
+        <div class="partnership-list">
+          <div class="">
+            <h3 class="font-[poppins] font-bold text-[clamp(28px,4vw,40px)] leading-[clamp(36px,5vw,48px)] tracking-[-0.02em] text-[#262626]"><?php echo $i?></h3>
+            <h4 class="font-[Poppins] font-medium text-[24px] leading-[32px] tracking-[-0.02em] md:text-[28px] md:leading-[36px] text-[#262626]"><?php echo get_sub_field();?></h4>
+            <p><?php echo get_sub_field();?></p>
+
           </div>
         </div>
-      </div>
+
+
+      </div> 
     </section>
 <?php endwhile;
 endif; ?>
