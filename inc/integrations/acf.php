7eeaf9de1f69b63ca2620c0bf4549e250aaaaece
devteam <development@alkalidesigns.com>
2026-03-23 18:33:35 +0530

    FAQ

--- inc/integrations/acf.php
+++ inc/integrations/acf.php
@@ -23,6 +23,7 @@ function register_acf_blocks()
   register_block_type(__DIR__ . '/acf-blocks/reviews-slider');
   register_block_type(__DIR__ . '/acf-blocks/partnership');
   register_block_type(__DIR__ . '/acf-blocks/case-studies');
+  register_block_type(__DIR__ . '/acf-blocks/faq');
   
   
   
