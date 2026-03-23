e4cd1721293f3c189c40d287d8b1bb523646bdd5
devteam <development@alkalidesigns.com>
2026-03-23 16:53:18 +0530

    Update services.php

--- inc/integrations/acf-blocks/services/services.php
+++ inc/integrations/acf-blocks/services/services.php
@@ -49,8 +49,8 @@ $wrapper_class = $select_3_column_grid
     : "grid grid-cols-[repeat(auto-fit,minmax(280px,1fr))] gap-4 md:gap-2 w-full";    
 
 $article_class = $select_3_column_grid
-    ? "source-card relative overflow-hidden w-full aspect-[16/15]"
-    : "source-card relative overflow-hidden w-full aspect-[334/420]";
+    ? "source-card relative overflow-hidden w-full aspect-[16/15] h-[420px] lg:h-auto"
+    : "source-card relative overflow-hidden w-full aspect-[334/420] ";
 
 
 $width = get_sub_field('select_short_content_width');
