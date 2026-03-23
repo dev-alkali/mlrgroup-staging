a3afe3af5c7b00a19b52de9c85d376bbc3c1c84d
nirav-e2m <development@alkalidesigns.com>
2026-03-23 19:26:46 +0530

    Update faq.php

--- inc/integrations/acf-blocks/faq/faq.php
+++ inc/integrations/acf-blocks/faq/faq.php
@@ -62,14 +62,14 @@ $mobileFlex = 'flex-col-reverse';
 						<div class="flex flex-col gap-[20px]">
 							<?php 
 							 $i = 1;
-							foreach($faq_lists as $faq $i++): ?>
+							foreach($faq_lists as $faq): ?>
 								<div class="flex flex-col bg-black text-white px-[28px] py-[36px] relative">
 									<h3 class="font-heading question font-medium text-white text-[clamp(18px,2.2vw,20px)] relative leading-[clamp(26px,2.6vw,28px)] pr-[50px]"><?php echo $i; ?> <?php echo $faq['question']; ?></h3>
 									<div class="answer pr-[50px]">
 										<div class="font-body font-normal text-white text-[clamp(14px,1.7vw,16px)] leading-[clamp(24px,2.8vw,26px)] "><?php echo $faq['answer']; ?></div>
 									</div>
 								</div>
-							<?php endforeach; ?>
+							<?php $i++; endforeach; ?>
 						</div>
 					<?php endif; ?>
 				</div>
