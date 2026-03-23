877e6498a4b3e900deae962527b652455acc87f8
nirav-e2m <development@alkalidesigns.com>
2026-03-23 15:11:24 +0530

    Update reviews-slider.php

--- inc/integrations/acf-blocks/reviews-slider/reviews-slider.php
+++ inc/integrations/acf-blocks/reviews-slider/reviews-slider.php
@@ -58,21 +58,21 @@ $mobileFlex = 'flex-col-reverse';
 							?>
 							<div class="review-slider-item swiper-slide !w-[1600px] max-w-[95%] md:max-w-[90%]">
 								<div class="flex flex-col md:flex-row p-[20px] md:p-[30px] lg:p-[40px] bg-black text-white gap-[32px] items-center relative">
-									<div class="top-arrow-imgs absolute top-0 right-0 md:w-1/2">
-										<img src="<?= get_template_directory_uri() ?>/assets/imgs/Arrow-thin-red.svg" alt="">
-										<img src="<?= get_template_directory_uri() ?>/assets/imgs/Arrow-thin.svg" alt="">
-										<img src="<?= get_template_directory_uri() ?>/assets/imgs/Arrow-thin.svg" alt="">
-										<img src="<?= get_template_directory_uri() ?>/assets/imgs/Arrow-thin.svg" alt="">
-										<img src="<?= get_template_directory_uri() ?>/assets/imgs/Arrow-thin.svg" alt="">
-										<img src="<?= get_template_directory_uri() ?>/assets/imgs/Arrow-thin.svg" alt="">										
+									<div class="top-arrow-imgs absolute top-0 right-[20px] md:w-[calc(50%-25px)]">
+										<img src="<?= get_template_directory_uri() ?>/assets/imgs/Arrow-thin-red.svg" alt="" class="w-[100%]">
+										<img src="<?= get_template_directory_uri() ?>/assets/imgs/Arrow-thin.svg" alt="" class="w-[100%]">
+										<img src="<?= get_template_directory_uri() ?>/assets/imgs/Arrow-thin.svg" alt="" class="w-[100%]">
+										<img src="<?= get_template_directory_uri() ?>/assets/imgs/Arrow-thin.svg" alt="" class="w-[100%]">
+										<img src="<?= get_template_directory_uri() ?>/assets/imgs/Arrow-thin.svg" alt="" class="w-[100%]">
+										<img src="<?= get_template_directory_uri() ?>/assets/imgs/Arrow-thin.svg" alt="" class="w-[100%]">										
 									</div>
-									<div class="bottom-arrow-imgs absolute bottom-0 right-0 md:w-1/2">
-										<img src="<?= get_template_directory_uri() ?>/assets/imgs/Arrow-thin.svg" alt="">
-										<img src="<?= get_template_directory_uri() ?>/assets/imgs/Arrow-thin.svg" alt="">
-										<img src="<?= get_template_directory_uri() ?>/assets/imgs/Arrow-thin.svg" alt="">
-										<img src="<?= get_template_directory_uri() ?>/assets/imgs/Arrow-thin.svg" alt="">
-										<img src="<?= get_template_directory_uri() ?>/assets/imgs/Arrow-thin.svg" alt="">										
-										<img src="<?= get_template_directory_uri() ?>/assets/imgs/Arrow-thin-red.svg" alt="">
+									<div class="bottom-arrow-imgs absolute bottom-0 right-20px md:w-[calc(50%-25px)]">
+										<img src="<?= get_template_directory_uri() ?>/assets/imgs/Arrow-thin.svg" alt="" class="w-[100%]">
+										<img src="<?= get_template_directory_uri() ?>/assets/imgs/Arrow-thin.svg" alt="" class="w-[100%]">
+										<img src="<?= get_template_directory_uri() ?>/assets/imgs/Arrow-thin.svg" alt="" class="w-[100%]">
+										<img src="<?= get_template_directory_uri() ?>/assets/imgs/Arrow-thin.svg" alt="" class="w-[100%]">
+										<img src="<?= get_template_directory_uri() ?>/assets/imgs/Arrow-thin.svg" alt="" class="w-[100%]">										
+										<img src="<?= get_template_directory_uri() ?>/assets/imgs/Arrow-thin-red.svg" alt="" class="w-[100%]">
 									</div>
 									<div class="w-full md:w-1/2 flex items-center justify-center">
 										<div class="w-full h-full pt-[100%] relative overflow-hidden">
