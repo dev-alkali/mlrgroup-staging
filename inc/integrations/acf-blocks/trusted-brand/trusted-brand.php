e2567c451c4a0736f3f15eff221b448d3c1a689c
nirav-e2m <development@alkalidesigns.com>
2026-03-24 16:44:46 +0530

    Update trusted-brand.php

--- inc/integrations/acf-blocks/trusted-brand/trusted-brand.php
+++ inc/integrations/acf-blocks/trusted-brand/trusted-brand.php
@@ -22,7 +22,7 @@ if (!empty($block['className'])) {
     <section id="<?php echo esc_attr($id); ?>" class="<?php echo esc_attr($className); ?> trusted_brand px-4 md:px-10 py-4 md:py-10">
       <div class="w-full wrapper relative">
         <?php if (have_rows('brands')) :  while (have_rows('brands')) : the_row(); ?>          
-            <div class="flex md:justify-start justify-center gap-2 md:gap-3 shrink-0 mb-[50px] items-center">
+            <div class="flex justify-start gap-2 md:gap-3 shrink-0 mb-[50px] items-center">
               <div class="relative "><img class="w-4 md:w-[27px] md:h-[27px]" src="<?= get_template_directory_uri() ?>/assets/imgs/Arrow-blue-brands.svg" alt="arrow"></div>
               <p class="text-[16px] font-medium md:text-[32px] md:tracking-[-2%] leading-6 md:leading-[40px] text-black font-heading"><?= wp_kses_post(get_sub_field('title')) ?></p>
             </div>
