eb0dd3be830b0edbc4cb8ba087df5245b252eac6
nirav-e2m <development@alkalidesigns.com>
2026-03-24 10:20:32 +0530

    Update 404.php

--- 404.php
+++ 404.php
@@ -8,11 +8,11 @@ get_header();
 
 		<section class="error-404 not-found">
 			<header class="page-header">
-				<h1 class="page-title text-[#262626] text-[clamp(96px,18vw,300px)] leading-[clamp(96px,18vw,300px)] tracking-[-0.02em]"><?php esc_html_e( '404 <span class="text-accent text-[clamp(64px,12vw,200px)] leading-[clamp(96px,18vw,300px)]">.</span>', 'devine' ); ?></h1>
+				<h1 class="page-title text-[#262626] text-[clamp(96px,18vw,300px)] leading-[clamp(96px,18vw,300px)] tracking-[-0.02em] text-center">404 <span class="text-accent text-[clamp(64px,12vw,200px)] leading-[clamp(96px,18vw,300px)]">.</span></h1>
 			</header><!-- .page-header -->
 
 			<div class="flex flex-col items-center justify-center gap-4">
-				<p class="text-[#525252] text-[clamp(18px,2.2vw,24px)] leading-[clamp(26px,2.8vw,32px)] tracking-[-0.02em] text-center"><?php esc_html_e( 'Sorry, we can’t find that page. But don’t worry, if you follow <a href="' . home_url() . '" class="text-accent">this link</a> <br> we can get you back to the homepage in no time.', 'devine' ); ?></p>
+				<p class="text-[#525252] text-[clamp(18px,2.2vw,24px)] leading-[clamp(26px,2.8vw,32px)] tracking-[-0.02em] text-center">Sorry, we can’t find that page. But don’t worry, if you follow <a href="<?php echo home_url(); ?>" class="text-accent">this link</a> <br> we can get you back to the homepage in no time.</p>
 			</div>
 		</section><!-- .error-404 -->
 
