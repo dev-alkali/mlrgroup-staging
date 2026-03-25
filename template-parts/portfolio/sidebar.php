a002f911f92d70812a3d4525ce9cf5c6575b8db4
devteam <development@alkalidesigns.com>
2026-03-25 19:02:27 +0530

    Update sidebar.php

--- template-parts/portfolio/sidebar.php
+++ template-parts/portfolio/sidebar.php
@@ -184,7 +184,7 @@ endif; ?>
 
 
 .filter-toggle-btn{display: inline-block;cursor: pointer;}
-.bar1, .bar2, .bar3 {width: 35px;height: 5px;background-color: #333;margin: 6px 0;transition: 0.4s;}
+.bar1, .bar2, .bar3 {width: 30px;height: 3px;background-color: #262626;margin: 6px 0;transition: 0.4s;}
 .change-btn .bar1 {transform: translate(0, 11px) rotate(-45deg);}
 .change-btn .bar2 {opacity: 0;}
 .change-btn .bar3 {transform: translate(0, -11px) rotate(45deg);}
