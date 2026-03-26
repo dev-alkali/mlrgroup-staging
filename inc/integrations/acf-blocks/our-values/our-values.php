a17ee066a37f83979e67b394e0a500fbcce8c2a8
nirav-e2m <development@alkalidesigns.com>
2026-03-26 10:07:02 +0530

    Update our-values.php

--- inc/integrations/acf-blocks/our-values/our-values.php
+++ inc/integrations/acf-blocks/our-values/our-values.php
@@ -20,80 +20,53 @@ if (!empty($block['className'])) {
 
 $title_row_1 = get_sub_field('title_row_1');
 $title_row_2 = get_sub_field('title_row_2');
-$description = get_sub_field('description');
-//$left_image_or_right_image = get_sub_field('left_image_or_right_image');
-$layout = get_sub_field('left_image_or_right_image'); // left / right
-// Desktop layout
-$desktopFlex = ($layout === 'Right Image') ? 'md:flex-row-reverse img-right' : 'md:flex-row img-left';
+$lists = get_sub_field('lists');
 
-// Mobile: always content first, image bottom
-$mobileFlex = 'flex-col-reverse';
 
-$content_width = get_sub_field('content_width');
-$section_color = get_sub_field('section_color');
 
-if ( $content_width ){
-  echo '<style>
-  @media (min-width: 1025px) {
-    .two-col-sec .description-content {
-      max-width: ' . $content_width . ';
-    }
-  }
-  </style>';
-}
-
-if ( $section_color == 'black' ){
-  $bg_color_class = 'bg-black';
-  $text_262626_class = 'text-white';
-  $text_737373_class = 'text-white';
-  $text_525252_class = 'text-white';
-  $text_e5e5e5_class = 'text-white';
-  $text_white_class = 'text-white';
-}elseif ( $section_color == 'white' ){
-  $bg_color_class = 'bg-white';
-  $text_262626_class = 'text-[#262626]';
-  $text_737373_class = 'text-[#737373]';
-  $text_525252_class = 'text-[#525252]';
-  $text_e5e5e5_class = 'text-[#E5E5E5]';
-  $text_white_class = 'text-white';
-}
 
 ?>
-    <section id="<?php echo esc_attr($id); ?>" class="<?php echo esc_attr($className); ?> two-col-sec px-4 md:px-10 py-[60px] md:py-[120px] <?php echo $bg_color_class; ?>">
-      <!-- <div class="gap-[30px] md:gap-[60px] w-full wrapper flex flex-col md:flex-row items-center flex-wrap"> -->
-      <div class="gap-[30px] md:gap-[0px] w-full wrapper flex flex-col <?php echo $mobileFlex . ' ' . $desktopFlex; ?> items-center">
+    <section id="<?php echo esc_attr($id); ?>" class="<?php echo esc_attr($className); ?> our-values-sec  px-4 md:px-10 py-[60px] md:py-[120px] <?php echo $bg_color_class; ?>">
+      
+      <div class="gap-[30px] md:gap-[0px] w-full wrapper flex flex-col">
 
-        <div class="w-full md:w-1/2 lg:w-[47%]">
-          <figure class="!m-0 flex"><?php echo wp_get_attachment_image(get_sub_field('image'), 'full', false, ['class' => 'w-full h-auto']); ?></figure>
-        </div>
+		<?php if($title_row_1 || $title_row_2 ): ?>
+			<div class="flex flex-col lg:flex-row gap-[20px] lg:gap-[50px] self-stretch w-full wrapper mb-[40px]">
+				<?php if($title_row_1 || $title_row_2): ?>
+				<h2 class="text-[clamp(32px,6vw,68px)] leading-[clamp(40px,7vw,76px)] tracking-[-4%] text-[#262626] font-heading lg:mb-[20px]">
+					<?php if($title_row_1): ?>
+						<span class="font-bold"><?= wp_kses_post($title_row_1) ?></span>
+					<?php endif; ?>
+					<?php if($title_row_2): ?>
+						<span class="font-light text-[#737373]"><?= wp_kses_post($title_row_2) ?></span>
+					<?php endif; ?>
+				</h2>
+				<?php endif; ?>
+			</div>
+		<?php endif; ?>
 
-        <div class="w-full md:w-1/2 lg:w-[53%]">
-          <div class="lg:px-[60px] <?php echo $layout === 'Right Image' ? 'pl-[0px] md:pr-[30px]' : 'pr-[0px] md:pl-[30px]'; ?>">
-            <?php if($title_row_1 || $title_row_2): ?>
-            <h2 class="text-[clamp(32px,6vw,68px)] leading-[clamp(40px,7vw,76px)] tracking-[-4%] <?php echo $text_262626_class; ?> font-heading mb-[20px]">
-              <?php if($title_row_1): ?>
-                  <span class="font-bold"><?= wp_kses_post($title_row_1) ?></span>
-              <?php endif; ?>
-              <?php if($title_row_2): ?>
-                  <span class="font-light <?php echo $text_737373_class; ?>"><?= wp_kses_post($title_row_2) ?></span>
-              <?php endif; ?>
-            </h2>
-            <?php endif; ?>
-            <?php if($description): ?>
-              <div class="w-full text-[clamp(16px,3vw,18px)] leading-[28px] <?php echo $text_525252_class; ?> font-body flex flex-col gap-[30px] description-content "><?= wp_kses_post($description) ?></div>
-            <?php endif; ?>
-
-            <?php 
-              $link = get_sub_field('button');
-              if( $link ): 
-                  $link_url = $link['url'];
-                  $link_title = $link['title'];
-                  $link_target = $link['target'] ? $link['target'] : '_self';
-                  ?>
-                  <a class="btn-primary mt-[40px]" href="<?php echo esc_url( $link_url ); ?>" target="<?php echo esc_attr( $link_target ); ?>"><?php echo esc_html( $link_title ); ?></a>
-              <?php endif; ?>
-          </div>
-        </div>
+		<?php if($lists): ?>
+			<div class="flex flex-col md:gap-[16px] gap-[10px]">
+				<?php foreach($lists as $list): 
+					$heading = $list['heading'];
+					$content = $list['content'];
+					?>
+					<div class=" flex flex-row gap-[20px]">
+						<div class="flex">
+							<img src="<?php get_template_directory_uri() ?>/assets/imgs/list-icon.svg" alt="" class="w-[50px] h-[50px]">
+						</div>
+						<div class="flex flex-col">
+							<?php if($heading): ?>
+								<h3 class="text-[clamp(20px,2.6vw,28px)] leading-[clamp(28px,3.2vw,36px)] tracking-[-0.02em] text-[#262626] font-heading"><?= wp_kses_post($list['title']) ?></h3>
+							<?php endif; ?>
+							<?php if($content): ?>
+								<div class="text-[clamp(16px,1.8vw,18px)] leading-[clamp(24px,2.2vw,26px)] text-[#525252] font-body"><?= wp_kses_post($list['content']) ?></div>
+							<?php endif; ?>
+						</div>
+					</div>
+				<?php endforeach; ?>
+			</div>
+		<?php endif; ?>
       </div>
     </section>
 <?php endwhile;
