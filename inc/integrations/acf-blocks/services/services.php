9b7d96a003555f857a555683dbe867518443f742
devteam <development@alkalidesigns.com>
2026-03-26 14:25:52 +0530

    Update services.php

--- inc/integrations/acf-blocks/services/services.php
+++ inc/integrations/acf-blocks/services/services.php
@@ -126,10 +126,10 @@ $max_width_class = ($width === 'Full') ? '' : 'max-w-[526px]';
             <div class="<?= esc_attr($wrapper_class) ?>">
               <?php if (have_rows('services')) :  while (have_rows('services')) : the_row(); ?>
                   <?php if (have_rows('service')) :  while (have_rows('service')) : the_row(); ?>
-                      <article class="<?= esc_attr($article_class) ?>">
-                        <div class="bg-image absolute inset-0" style=" background-image: url('<?php echo esc_url(get_sub_field('image')); ?>'); background-position: center center; background-size: cover; background-repeat: no-repeat; filter: grayscale(100%); ">
-                        </div>           
-                        <a href="<?= esc_url(get_sub_field('link_path')) ?>" class="gradient-box absolute flex flex-col flex-1 justify-between px-5 md:px-6 py-7 w-full h-full">
+                      <article class="<?= esc_attr($article_class) ?>" style=" background-image: url('<?php echo esc_url(get_sub_field('image')); ?>'); background-position: center center; background-size: cover; background-repeat: no-repeat; filter: grayscale(100%); ">
+                        <!-- <div class="bg-image absolute inset-0" >
+                        </div>            -->
+                        <a href="<?= esc_url(get_sub_field('link_path')) ?>" class="gradient-box flex flex-col flex-1 justify-between px-5 md:px-6 py-7 w-full h-full">
                           <img class="arrow absolute w-10 z-10" src="<?= get_template_directory_uri() ?>/assets/imgs/Arrow.svg" alt="" />
                           <div class="flex flex-col gap-3 md:gap-4 content z-10">
                             <h3 class="text-white card-title"><?= wp_kses_post(get_sub_field('title')) ?></h3>
