74e9d764a41faea4396eb6612816863c76986a39
devteam <development@alkalidesigns.com>
2026-03-27 12:57:06 +0530

    PHPO

--- template-parts/case-study/case-study-hero.php
+++ template-parts/case-study/case-study-hero.php
@@ -6,8 +6,8 @@ if (have_rows('hero_case_study', 'option')) : while (have_rows('hero_case_study'
   $title_row_1 = get_sub_field('title_row_1');
   $title_row_2 = get_sub_field('title_row_2');
   $description = get_sub_field('description');
-  $bg_desktop_img = get_sub_field('bg_image');
-  $bg_mobile_image = get_sub_field('bg_mobile_image');
+  $bg_desktop_img = get_sub_field('case_study_bg_image');
+  $bg_mobile_image = get_sub_field('case_study_bg_mobile_image');
 ?>
     <section class="case-study-hero w-full px-4 min-[600px]:px-10 min-[767px]:px-20 lg:px-[100px] bg-center bg-cover bg-no-repeat bg-[rgba(0,0,0,0.5)] bg-blend-overlay" style="--bg-desktop: url('<?php echo esc_url($bg_desktop_img); ?>');<?php if($bg_mobile_image): ?>--bg-mobile: url('<?php echo esc_url($bg_mobile_image); ?>');<?php endif; ?>">
       <div class="gap-10 w-full wrapper min-h-screen md:min-h-[670px] pt-[80px] md:pt-[118px] pb-[80px] md:pb-[60px] flex items-end !px-0">
