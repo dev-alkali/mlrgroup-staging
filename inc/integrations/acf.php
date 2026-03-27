c6bc6d9ad0c7c8c9e8f6c695102e73eb86163f03
nirav-e2m <development@alkalidesigns.com>
2026-03-27 17:07:18 +0530

    Update acf.php

--- inc/integrations/acf.php
+++ inc/integrations/acf.php
@@ -26,6 +26,7 @@ function register_acf_blocks()
   register_block_type(__DIR__ . '/acf-blocks/faq');
   register_block_type(__DIR__ . '/acf-blocks/our-values');
   register_block_type(__DIR__ . '/acf-blocks/faq-columns');
+  register_block_type(__DIR__ . '/acf-blocks/content');
   
   
   
