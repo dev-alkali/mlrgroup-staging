d51c6844409e1ef8b5ae12486d82c1895d0de3b8
devteam <development@alkalidesigns.com>
2026-03-30 15:53:54 +0530

    Update map.php

--- inc/integrations/acf-blocks/map/map.php
+++ inc/integrations/acf-blocks/map/map.php
@@ -21,8 +21,8 @@ $title2 = get_sub_field('title_row_2');
 $map_code = get_sub_field('map_code');
 
 ?>
-<section id="<?php echo esc_attr($id); ?>" class="<?php echo esc_attr($className); ?> map-sec px-4 md:px-10 py-[60px] lg:py-[80px] xl:py-[120px] overflow-hidden">
-  <div class="wrapper">
+<section id="<?php echo esc_attr($id); ?>" class="<?php echo esc_attr($className); ?> map-sec py-[60px] lg:py-[80px] xl:py-[120px] overflow-hidden">
+  <div class="wrapper px-4 md:px-10">
       <?php if ($title1 || $title2) : ?>
         <div class="serve-heading xl:mb-[80px] lg:mb-[50px] mb-[32px]">
           <h2 class=" font-heading font-bold text-[clamp(36px,4.5vw,68px)] leading-[clamp(44px,5.2vw,78px)] tracking-[-0.02em]">
