0c7e392c914b6bf5433e1c7e6238098f3622c3e5
devteam <development@alkalidesigns.com>
2026-03-30 18:34:06 +0530

    Update lookbooks-list.php

--- inc/integrations/acf-blocks/lookbooks-list/lookbooks-list.php
+++ inc/integrations/acf-blocks/lookbooks-list/lookbooks-list.php
@@ -23,7 +23,7 @@ if (!empty($block['className'])) {
         <?php if ( have_rows( 'lookbooks' ) ) : ?>
           <?php while ( have_rows( 'lookbooks' ) ) : the_row(); ?>
           <?php $year = get_sub_field( 'year' ); ?>
-      <div class="lookbook-year mb-[60px] md:mb-[80px]">
+      <div class="lookbook-year mb-[25px] md:mb-[45px]">
 
         <h2 class="font-[poppins] font-bold text-[#262626] leading-[clamp(44px,4vw,60px)] text-[clamp(36px,5vw,60px)] tracking-[-0.02em] mb-[17px] md:mb-[21px] flex ga-[12px] md:gap-[16px]"><img src="<?= get_template_directory_uri() ?>/assets/imgs/Arrow-red.svg" alt="" class="w-[35px] h-[35px] md:w-[62px] md:h-[62px]"> <?php echo esc_html( $year ); ?></h2>
 
