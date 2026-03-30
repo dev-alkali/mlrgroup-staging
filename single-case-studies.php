e5574b6d37df6987a8a5d30a51c4d48d6e8d9796
nirav-e2m <development@alkalidesigns.com>
2026-03-30 16:15:16 +0530

    Update single-case-studies.php

--- single-case-studies.php
+++ single-case-studies.php
@@ -45,8 +45,8 @@ get_header();
           </div>
         <?php endif; ?>
 
-        <article class="mt-[28px] md:px-[120px]">
-          <div class="">
+        <article class="md:my-[120px] my-[60px] md:px-[120px]">
+          <div class="mb-[20px]">
             <h2 class="font-poppins font-bold text-[clamp(28px,4vw,40px)] leading-[clamp(36px,4.5vw,48px)] tracking-[-0.02em] text-[#262626]">Summary:</h2>
           </div>
           <div class="blog-content">  
