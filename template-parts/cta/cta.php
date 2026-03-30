b779a8edf61decd2848e51126edbf477dfc03f3d
devteam <development@alkalidesigns.com>
2026-03-30 19:28:08 +0530

    Update cta.php

--- template-parts/cta/cta.php
+++ template-parts/cta/cta.php
@@ -47,7 +47,7 @@ $link = get_sub_field('cta_button');
                 $link_title = $link['title'];
                 $link_target = $link['target'] ? $link['target'] : '_self';
                 ?>
-                <a class="btn-primary" href="<?php echo esc_url( $link_url ); ?>" target="<?php echo esc_attr( $link_target ); ?>"><?php echo esc_html( $link_title ); ?></a>
+                <a class="btn-primary" href="<?php echo esc_url( $link_url ); ?>" target="<?php echo esc_attr( $link_target ); ?>"><?php echo esc_html( $link_title ); ?> TEST</a>
             <?php endif; ?>
         </div>
       </div>
