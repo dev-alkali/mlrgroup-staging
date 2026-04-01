5848af4d1a62fc756a2a4d7799d84ad847599246
devteam <development@alkalidesigns.com>
2026-04-01 19:00:16 +0530

    Update services.php

--- inc/integrations/acf-blocks/services/services.php
+++ inc/integrations/acf-blocks/services/services.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * Services Block Template.
  */
@@ -40,29 +39,6 @@ if ($card_overlay === 'Grayscale') {
 
 $select_3_column_grid = get_sub_field('select_3_column_grid');
 $select_3_column_grid = get_sub_field('select_3_column_grid');
-
-// $wrapper_class = $select_3_column_grid
-//     ? "grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4 md:gap-2 w-full"
-//     : "grid grid-cols-[repeat(auto-fit,minmax(280px,1fr))] gap-4 md:gap-2 w-full";
-
-
-
-// $wrapper_class = $select_3_column_grid
-//     ? "grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4 md:gap-2 w-full [&>article:last-child:nth-child(odd)]:md:col-span-2 [&>article:last-child:nth-child(odd)]:xl:col-span-1"
-//     : "grid grid-cols-[repeat(auto-fit,minmax(280px,1fr))] gap-4 md:gap-2 w-full";    
-
-// $article_class = $select_3_column_grid
-//     ? "source-card relative overflow-hidden w-full aspect-[16/15] h-[420px] xl:h-auto"
-//     : "source-card relative overflow-hidden w-full aspect-[334/420] ";
-
-
-// $wrapper_class = $select_3_column_grid
-//     ? "grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 [&:has(>article:nth-child(2):last-child)]:xl:grid-cols-2 gap-4 md:gap-2 w-full [&>article:last-child:nth-child(odd)]:md:col-span-2 [&>article:last-child:nth-child(odd)]:xl:col-span-1"
-//     : "grid grid-cols-[repeat(auto-fit,minmax(280px,1fr))] gap-4 md:gap-2 w-full";
-
-// $article_class = $select_3_column_grid
-//     ? "source-card relative overflow-hidden w-full aspect-[16/15] h-[420px] max-h-[420px] xl:h-auto"
-//     : "source-card relative overflow-hidden w-full aspect-[334/420]";
 
 $wrapper_class = $select_3_column_grid
     ? "grid grid-cols-1 md:grid-cols-2 xl:grid-cols-6 gap-4 md:gap-2 w-full [&>article]:xl:col-span-2 [&:has(>article:nth-child(2):last-child)>article]:xl:col-span-3 [&:has(>article:nth-child(5):last-child)>article:nth-child(n+4)]:xl:col-span-3 [&>article:last-child:nth-child(odd)]:md:col-span-2"
@@ -123,10 +99,16 @@ $max_width_class = ($width === 'Full') ? '' : 'max-w-[526px]';
         <?php endwhile;
       endif; ?>
 
-			<?php if (have_rows('main_content')) :  while (have_rows('main_content')) : the_row();
-				// Each repeater row in 'services' = 1 card (service is a group, not a repeater)
-				$services_rows = get_sub_field('services');
-				$total_cards = is_array($services_rows) ? count($services_rows) : 0;
+<?php if (have_rows('main_content')) :  while (have_rows('main_content')) : the_row();
+        $content_mode      = strtolower((string) get_sub_field('select_services_or_manual_content'));
+        $selected_services = get_sub_field('select_services');
+        $manual_services   = get_sub_field('services');
+        $use_services      = ($content_mode === 'services' && !empty($selected_services));
+
+				$total_cards = $use_services
+          ? (is_array($selected_services) ? count($selected_services) : 0)
+          : (is_array($manual_services) ? count($manual_services) : 0);
+
 				$extra_class = '';
 				if ($total_cards === 5) {
 					$extra_class = ' services-grid-5';
@@ -146,26 +128,55 @@ $max_width_class = ($width === 'Full') ? '' : 'max-w-[526px]';
 
 
             <div class="<?= esc_attr($grid_class) ?>">
-              <?php if (have_rows('services')) :  while (have_rows('services')) : the_row(); ?>
-                  <?php if (have_rows('service')) :  while (have_rows('service')) : the_row(); ?>
-                      <article class="<?= esc_attr($article_class) ?>">
-                        <div class="bg-image absolute inset-0 z-0" style=" background-image: url('<?php echo esc_url(get_sub_field('image')); ?>'); background-position: center center; background-size: cover; background-repeat: no-repeat; filter:grayscale(100%);">
-                        </div>
-                        <a href="<?= esc_url(get_sub_field('link_path')) ?>" class="gradient-box flex flex-col flex-1 justify-between px-5 md:px-6 py-7 w-full h-full relative z-1">
-                          <figure class="text-right z-10 relative">
-                            <img class="arrow w-10" src="<?= get_template_directory_uri() ?>/assets/imgs/Arrow.svg" alt="" />
-                          </figure>
-                          <div class="flex flex-col gap-3 md:gap-4 content z-10">
-                            <h3 class="text-white card-title"><?= wp_kses_post(get_sub_field('title')) ?></h3>
-                            <p class="text-white text-[16px] md:text-lg leading-[26px] md:leading-7 font-body card-text"><?= wp_kses_post(get_sub_field('paragraph')) ?></p>
-                          </div>
-                        </a>
-                      </article>
+              <?php if ($use_services) : ?>
+                <?php foreach ($selected_services as $service_post) : ?>
+                  <?php
+                  $service_id = is_object($service_post) ? $service_post->ID : (int) $service_post;
+                  if (!$service_id) {
+                    continue;
+                  }
 
-                  <?php endwhile;
-                  endif; ?>
-              <?php endwhile;
-              endif; ?>
+                  $card_link = get_permalink($service_id);
+                  $card_image = get_the_post_thumbnail_url($service_id, 'full');
+                  $card_title = get_the_title($service_id);
+                  $card_paragraph = get_field('short_content', $service_id);
+                  ?>
+                  <article class="<?= esc_attr($article_class) ?>">
+                    <div class="bg-image absolute inset-0 z-0" style=" background-image: url('<?php echo esc_url($card_image); ?>'); background-position: center center; background-size: cover; background-repeat: no-repeat; filter:grayscale(100%);">
+                    </div>
+                    <a href="<?= esc_url($card_link) ?>" class="gradient-box flex flex-col flex-1 justify-between px-5 md:px-6 py-7 w-full h-full relative z-1">
+                      <figure class="text-right z-10 relative">
+                        <img class="arrow w-10" src="<?= get_template_directory_uri() ?>/assets/imgs/Arrow.svg" alt="" />
+                      </figure>
+                      <div class="flex flex-col gap-3 md:gap-4 content z-10">
+                        <h3 class="text-white card-title"><?= esc_html($card_title) ?></h3>
+                        <p class="text-white text-[16px] md:text-lg leading-[26px] md:leading-7 font-body card-text"><?= wp_kses_post($card_paragraph) ?></p>
+                      </div>
+                    </a>
+                  </article>
+                <?php endforeach; ?>
+              <?php else : ?>
+                <?php if (have_rows('services')) :  while (have_rows('services')) : the_row(); ?>
+                    <?php if (have_rows('service')) :  while (have_rows('service')) : the_row(); ?>
+                        <article class="<?= esc_attr($article_class) ?>">
+                          <div class="bg-image absolute inset-0 z-0" style=" background-image: url('<?php echo esc_url(get_sub_field('image')); ?>'); background-position: center center; background-size: cover; background-repeat: no-repeat; filter:grayscale(100%);">
+                          </div>
+                          <a href="<?= esc_url(get_sub_field('link_path')) ?>" class="gradient-box flex flex-col flex-1 justify-between px-5 md:px-6 py-7 w-full h-full relative z-1">
+                            <figure class="text-right z-10 relative">
+                              <img class="arrow w-10" src="<?= get_template_directory_uri() ?>/assets/imgs/Arrow.svg" alt="" />
+                            </figure>
+                            <div class="flex flex-col gap-3 md:gap-4 content z-10">
+                              <h3 class="text-white card-title"><?= wp_kses_post(get_sub_field('title')) ?></h3>
+                              <p class="text-white text-[16px] md:text-lg leading-[26px] md:leading-7 font-body card-text"><?= wp_kses_post(get_sub_field('paragraph')) ?></p>
+                            </div>
+                          </a>
+                        </article>
+
+                    <?php endwhile;
+                    endif; ?>
+                <?php endwhile;
+                endif; ?>
+              <?php endif; ?>
             </div>
 
         <?php endwhile;
