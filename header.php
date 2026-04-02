4c70a40346b89f61bfdb63b6a875cac2046b5357
devteam <development@alkalidesigns.com>
2026-04-02 18:52:42 +0530

    Update header.php

--- header.php
+++ header.php
@@ -36,7 +36,7 @@ if ( $is_transparent_page ) {
 ?>
 <?php //$header_schema = get_field('header_schema') == 'light-skin' ? 'absolute min-[1024px]:pt-[52px]' : 'relative dark-skin min-[1024px]:py-4';?>
 
-<header class=" <?php echo $header_schema.' '.get_field('header_schema'); ?> z-[99] w-full site-header px-4 min-[600px]:px-10 min-[767px]:px-20 lg:px-[100px]">
+<header class=" <?php echo $header_schema.' '.get_field('header_schema'); ?> z-[99] w-full site-header anim px-4 min-[600px]:px-10 min-[767px]:px-20 lg:px-[100px]" data-delay="2.25" data-anim="up" data-start="top 100%">
   
   <?php
   $locations = get_nav_menu_locations();
