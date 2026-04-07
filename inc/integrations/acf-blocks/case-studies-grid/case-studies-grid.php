9dca9a96174d3506e4857504024b48b871f237e2
Dev Team <development@alkalidesigns.com>
2026-04-07 15:25:53 +0530

    Update case-studies-grid.php

--- inc/integrations/acf-blocks/case-studies-grid/case-studies-grid.php
+++ inc/integrations/acf-blocks/case-studies-grid/case-studies-grid.php
@@ -16,10 +16,10 @@ if (!empty($block['className'])) {
 ?>
 <?php if (have_rows('case_studies_grid')) :  while (have_rows('case_studies_grid')) : the_row(); ?>
     <section id="<?php echo esc_attr($id); ?>" class="<?php echo esc_attr($className); ?> flex justify-center pt-20 px-0 bg-white">    
-        <div class="flex flex-col w-full items-center gap-10">
+        <div class="max-w-[1920px]">
         <!-- Heading -->
-        <div class="max-w-[1920px] w-full flex flex-col items-start justify-center gap-5 px-4 sm:px-10">            
-            <h2 class="flex flex-col max-w-[512px] font-heading text-black tracking-[-0.02em] text-[clamp(36px,5vw,68px)] leading-[clamp(44px,6vw,80px)]">
+        <div class="w-full flex flex-col items-start justify-center gap-5 px-4 sm:px-10">            
+            <h2 class="flex flex-col max-w-[512px] font-heading text-white tracking-[-0.02em] text-[clamp(36px,5vw,68px)] leading-[clamp(44px,6vw,78px)]">
                 <span class="font-bold">
                     <?= wp_kses_post(get_sub_field('title_row_1')) ?>
                 </span>
@@ -30,18 +30,21 @@ if (!empty($block['className'])) {
         </div>
 
         <!-- Case Studies Grid -->
-        <div class="w-full bg-white grid grid-cols-2 lg:grid-cols-4">
+        <div class="w-full flex">
             <?php if (have_rows('works_rows')) : while (have_rows('works_rows')) : the_row(); ?>
                 <?php if (have_rows('works')) : while (have_rows('works')) : the_row(); ?>
                     <a href="<?php echo esc_url(get_sub_field('item_path')); ?>"
-                       class="work-card w-full h-[240px] sm:h-[300px] md:h-auto md:aspect-[360/400]"
+                       class="cs-card w-full h-[240px] sm:h-[300px] md:h-auto md:aspect-[360/400]"
                        style="
                             background-image: url('<?php echo esc_url(get_sub_field('bg_image')); ?>');
                             background-position: 50% 50%;
                             background-size: cover;
                             background-repeat: no-repeat;">
                         <div class="gradient-box flex flex-col items-start justify-end gap-2 px-3 sm:px-6 py-4 sm:py-7 h-full">
-                            <div class="content flex items-center justify-end gap-1 sm:gap-2 w-full">
+                            <div clas="cs-card__logo">
+                                <?php echo get_sub_field('logo_image'); ?>
+                            </div>
+                            <div class="cs-card__content flex items-center justify-end gap-1 sm:gap-2 w-full">
                                 <div class="flex-1 text-white uppercase font-heading font-semibold text-[clamp(12px,1.4vw,18px)] leading-[clamp(18px,2vw,28px)] whitespace-nowrap">                                    
                                     <?= wp_kses_post(get_sub_field('title')) ?>
                                 </div>
@@ -52,6 +55,17 @@ if (!empty($block['className'])) {
                 <?php endwhile; endif; ?>
             <?php endwhile; endif; ?>
         </div>
+
+        <?php
+            $view_more = get_sub_field('view_more');
+            if($view_more):
+        ?>
+           <a class="flex items-center" href="<?php echo $view_more['url']; ?>" target="<?php echo $view_more['target']; ?>">
+            <span><?php echo $view_more['title']; ?></span>
+            <svg width="16" height="16" viewBox="0 0 16 16" fill="none" stroke="#FD4338" xmlns="http://www.w3.org/2000/svg">
+            <path d="M2.26562 2.47656H13.407V13.9386" stroke-miterlimit="10"/><path d="M13.3351 2.54688L2.33789 13.8605" stroke-miterlimit="10"/> </svg></a>
+        <?php endif; ?>
+
     </div>
 </section>
 
