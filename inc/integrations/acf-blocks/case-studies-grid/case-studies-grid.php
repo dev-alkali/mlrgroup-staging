2b118560a3e0349004b91d3b8290186b61fdb3ab
Dev Team <development@alkalidesigns.com>
2026-04-07 16:44:55 +0530

    Update case-studies-grid.php

--- inc/integrations/acf-blocks/case-studies-grid/case-studies-grid.php
+++ inc/integrations/acf-blocks/case-studies-grid/case-studies-grid.php
@@ -33,25 +33,26 @@ if (!empty($block['className'])) {
         <div class="w-full flex cs-cards py-[clamp(24px, 4vw, 60px)] <?php echo count(get_sub_field('works_rows')) ?>">
             <?php if (have_rows('works_rows')) : while (have_rows('works_rows')) : the_row(); ?>
                 <?php if (have_rows('works')) : while (have_rows('works')) : the_row(); ?>
+                   
                     <a href="<?php echo esc_url(get_sub_field('item_path')); ?>"
-                       class="cs-card w-full h-[240px] sm:h-[300px] md:h-auto md:aspect-[360/400]"
+                       class="cs-card w-full"
                        style="
                             background-image: url('<?php echo esc_url(get_sub_field('bg_image')); ?>');
                             background-position: 50% 50%;
                             background-size: cover;
                             background-repeat: no-repeat;">
                             
-                        <div class="gradient-box flex flex-col items-center justify-center gap-2 px-3 sm:px-6 py-4 sm:py-6 h-full">
+                        <div class="gradient-box h-full w-full">
                             
                             <img class="arrow relative w-[clamp(24px, 4vw, 40px)] h-[clamp(24px, 4vw, 40px)]" src="<?= get_template_directory_uri() ?>/assets/imgs/Arrow.svg" alt="Arrow">
 
-                            <div class="cs-card__content flex items-center justify-end gap-1 sm:gap-2 w-full">
+                            <div class="cs-card__content w-full">
                                 <?php if(get_sub_field('logo_image')): ?>
                                     <div class="cs-card__logo">
                                         <img src="<?php echo get_sub_field('logo_image'); ?>" alt="">
                                     </div>
                                 <?php endif; ?>
-                                <div class="flex-1 text-white uppercase font-heading font-semibold text-[clamp(14px,1.64vw,24px)] leading-[clamp(18px,2.25vw,32px)] whitespace-nowrap">                                    
+                                <div class="flex-1 text-white uppercase font-heading font-semibold text-[clamp(16px,1.64vw,24px)] leading-[clamp(22px,2.25vw,32px)] whitespace-nowrap">                                    
                                     <?= wp_kses_post(get_sub_field('title')) ?>
                                 </div>
                             </div>
@@ -66,7 +67,7 @@ if (!empty($block['className'])) {
             $view_more = get_sub_field('view_more');
             if($view_more):
         ?>
-           <a class="flex case-studies-grid-link items-center text-[] font-heading font-semibold " href="<?php echo $view_more['url']; ?>" target="<?php echo $view_more['target']; ?>">
+           <a class="flex case-studies-grid-link items-center text-[16px] leading-[24px] font-heading font-semibold " href="<?php echo $view_more['url']; ?>" target="<?php echo $view_more['target']; ?>">
             <span><?php echo $view_more['title']; ?></span>
             <svg width="16" height="16" viewBox="0 0 16 16" fill="none" stroke="#FD4338" xmlns="http://www.w3.org/2000/svg">
             <path d="M2.26562 2.47656H13.407V13.9386" stroke-miterlimit="10"/><path d="M13.3351 2.54688L2.33789 13.8605" stroke-miterlimit="10"/> </svg></a>
