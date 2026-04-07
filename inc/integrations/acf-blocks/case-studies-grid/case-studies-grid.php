c99d1b79728c41ae3a366f7cdbbd5596bf077d6c
Dev Team <development@alkalidesigns.com>
2026-04-07 15:56:59 +0530

    Update case-studies-grid.php

--- inc/integrations/acf-blocks/case-studies-grid/case-studies-grid.php
+++ inc/integrations/acf-blocks/case-studies-grid/case-studies-grid.php
@@ -15,11 +15,11 @@ if (!empty($block['className'])) {
 }
 ?>
 <?php if (have_rows('case_studies_grid')) :  while (have_rows('case_studies_grid')) : the_row(); ?>
-    <section id="<?php echo esc_attr($id); ?>" class="<?php echo esc_attr($className); ?> flex justify-center pt-20 px-0 bg-white">    
+    <section id="<?php echo esc_attr($id); ?>" class="<?php echo esc_attr($className); ?> flex justify-center pt-20 px-0 bg-black">    
         <div class="max-w-[1920px]">
         <!-- Heading -->
         <div class="w-full flex flex-col items-start justify-center gap-5 px-4 sm:px-10">            
-            <h2 class="flex flex-col max-w-[512px] font-heading text-white tracking-[-0.02em] text-[clamp(36px,5vw,68px)] leading-[clamp(44px,6vw,78px)]">
+            <h2 class="flex flex-col max-w-[660px] font-heading text-black tracking-[-0.02em] text-[clamp(36px,5vw,68px)] leading-[clamp(44px,6vw,78px)]">
                 <span class="font-bold">
                     <?= wp_kses_post(get_sub_field('title_row_1')) ?>
                 </span>
@@ -41,16 +41,16 @@ if (!empty($block['className'])) {
                             background-size: cover;
                             background-repeat: no-repeat;">
                             
-                        <div class="gradient-box flex flex-col items-start justify-end gap-2 px-3 sm:px-6 py-4 sm:py-7 h-full">
+                        <div class="gradient-box flex flex-col items-center justify-center gap-2 px-3 sm:px-6 py-4 sm:py-6 h-full">
                             
                             <?php if(get_sub_field('logo_image')): ?>
-                                <div clas="cs-card__logo">
+                                <div class="cs-card__logo">
                                     <img src="<?php echo get_sub_field('logo_image'); ?>" alt="">
                                 </div>
                             <?php endif; ?>
 
                             <div class="cs-card__content flex items-center justify-end gap-1 sm:gap-2 w-full">
-                                <div class="flex-1 text-white uppercase font-heading font-semibold text-[clamp(12px,1.4vw,18px)] leading-[clamp(18px,2vw,28px)] whitespace-nowrap">                                    
+                                <div class="flex-1 text-white uppercase font-heading font-semibold text-[clamp(14px,1.64vw,24px)] leading-[clamp(18px,2.25vw,32px)] whitespace-nowrap">                                    
                                     <?= wp_kses_post(get_sub_field('title')) ?>
                                 </div>
                                 <img class="arrow relative w-[11px] sm:w-[20px] xl:w-[24px] h-[10px] sm:h-5 xl:h-6 mt-[-2px]" src="<?= get_template_directory_uri() ?>/assets/imgs/Arrow.svg" alt="Arrow">
