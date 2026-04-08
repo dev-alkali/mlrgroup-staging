8f6a2f6507c2b2c05f34fbb0f7040d4df11d3699
devteam <development@alkalidesigns.com>
2026-04-08 16:23:56 +0530

    Update performance.php

--- inc/integrations/acf-blocks/performance/performance.php
+++ inc/integrations/acf-blocks/performance/performance.php
@@ -47,7 +47,7 @@ if (!empty($block['className'])) {
                     </div>
                 </div>
 
-                <div class="w-full flex justify-end" style="background-image:url(<?= get_template_directory_uri() ?>/assets/imgs/Arrow-white-icon.svg);background-position: center left;background-size: 50px;background-repeat:repeat-x;">                    
+                <div class="w-full flex justify-end" style="background-image:url(<?= get_template_directory_uri() ?>/assets/imgs/Arrow-white-icon.svg);background-position: center right;background-size: 50px;background-repeat:repeat-x;">                    
                     <img src="<?= get_template_directory_uri() ?>/assets/imgs/Arrow-red-icon.svg" class="arrow1 w-[clamp(30px,7vw,50px)] h-[clamp(30px,7vw,50px)] bg-black" alt="">
                 </div>
             </div>
