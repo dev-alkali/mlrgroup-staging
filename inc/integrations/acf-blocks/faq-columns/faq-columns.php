edaf6783de19946f6178bacac8e90c2374dd512e
devteam <development@alkalidesigns.com>
2026-04-08 12:26:47 +0530

    Update faq-columns.php

--- inc/integrations/acf-blocks/faq-columns/faq-columns.php
+++ inc/integrations/acf-blocks/faq-columns/faq-columns.php
@@ -56,8 +56,8 @@ $faq_lists = get_sub_field('faq_lists');
 								$question = ! empty( $faq['question'] ) ? $faq['question'] : '';
 								$answer   = ! empty( $faq['answer'] ) ? $faq['answer'] : '';
 							?>
-								<div class="flex flex-col bg-black text-white xl:py-[32px] lg:py-[24px] py-[20px] px-[6px] relative border-b border-[#404040] md:first:border-t border-solid">
-									<h3 class="font-heading question font-medium text-white text-[clamp(16px,2.2vw,20px)] relative leading-[clamp(24px,2.6vw,28px)] pr-[50px] flex gap-[12px] items-start cursor-pointer">
+								<div class="flex flex-col bg-black text-white px-[6px] relative border-b border-[#404040] md:first:border-t border-solid">
+									<h3 class="font-heading question font-medium text-white text-[clamp(16px,2.2vw,20px)] relative leading-[clamp(24px,2.6vw,28px)] xl:py-[32px] lg:py-[24px] py-[20px] pr-[50px] flex gap-[12px] items-start cursor-pointer ">
 										<img src="<?= get_template_directory_uri() ?>/assets/imgs/faq-title-arrow.svg" alt="" class="faq-title-arrow w-[21px] h-[21px]"><?php echo $question; ?>
 									</h3>
 									<div class="answer md:pr-[50px] pr-[30px] pl-[34px]" role="region">
