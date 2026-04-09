a16085ca6cb319bcdecded0fbf9ebc0039711d50
devteam <development@alkalidesigns.com>
2026-04-09 16:13:17 +0530

    Update footer.php

--- footer.php
+++ footer.php
@@ -8,11 +8,11 @@
       <div class="flex flex-col max-w-[1920px] items-start gap-10 w-full">
         <div class="flex flex-col min-[1199px]:flex-row w-full lg:justify-between items-start gap-15 min-[1280px]:gap-28">
           <!-- Brand -->
-          <div class="flex flex-col lg:flex-row max-w-[305px] lg:border-r lg:border-white w-full items-start gap-5 lg:align-center">
-            <a class="relative max-w-[250px] overflow-hidden flex" href="<?= home_url(); ?>">
+          <div class="flex flex-col lg:flex-row max-w-[305px] w-full items-start gap-5 lg:align-center">
+            <a class="relative max-w-[250px] overflow-hidden flex " href="<?= home_url(); ?>" >
               <img class="w-full max-w-[250px] h-[61.8px]" src="<?= esc_url(get_field('footer_logo', 'option')); ?>" alt="logo">
             </a>
-            <div class="flex gap-4 items-center">
+            <div class="flex gap-4 items-center lg:border-l lg:border-white pl-2 ml-2">
               <?php if (have_rows('social_links', 'option')): while (have_rows('social_links', 'option')): the_row(); ?>
                 <a href="<?= esc_url(get_sub_field('link')) ?>">
                   <img src="<?= esc_url(get_sub_field('icon')) ?>" alt="" />
