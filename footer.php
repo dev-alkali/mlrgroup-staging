5711a5f0a83e06dbe23d5823dc7486fd09a68d57
devteam <development@alkalidesigns.com>
2026-04-09 16:28:28 +0530

    Update footer.php

--- footer.php
+++ footer.php
@@ -10,7 +10,7 @@
           <!-- Brand -->
           <div class="max-w-[460px] w-full items-start">
             <div class="flex flex-col lg:flex-row items-start lg:items-center mb-[20px]">
-              <a class="relative max-w-[220px] overflow-hidden " href="<?= home_url(); ?>" >
+              <a class="relative max-w-[180px] md:max-w-[200px] lg:max-w-[220px] overflow-hidden " href="<?= home_url(); ?>" >
                 <img class="w-full" src="<?= esc_url(get_field('footer_logo', 'option')); ?>" alt="logo">
               </a>
               <div class="flex gap-4 items-center lg:border-l lg:border-white pl-2 ml-2">
