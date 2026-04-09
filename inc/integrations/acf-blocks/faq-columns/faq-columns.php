b19ed9b40b679d9ff177c479b79b5a25c56bd336
devteam <development@alkalidesigns.com>
2026-04-09 17:11:39 +0530

    FAQ Update

--- inc/integrations/acf-blocks/faq-columns/faq-columns.php
+++ inc/integrations/acf-blocks/faq-columns/faq-columns.php
@@ -70,11 +70,11 @@ if ( ! empty( $section_remove_bottom_padding ) ) {
 								$answer   = ! empty( $faq['answer'] ) ? $faq['answer'] : '';
 							?>
 								<div class="flex flex-col bg-black text-white px-[6px] relative border-b border-[#404040] md:first:border-t border-solid">
-									<h3 class="font-heading question font-medium text-white text-[clamp(16px,2.2vw,20px)] relative leading-[clamp(24px,2.6vw,28px)] xl:py-[32px] lg:py-[24px] py-[20px] pr-[50px] flex gap-[12px] items-start cursor-pointer ">
+									<h3 class="font-heading question font-medium text-white text-[clamp(16px,2.2vw,20px)] relative leading-[clamp(24px,2.6vw,28px)] xl:py-[24px] lg:py-[24px] py-[20px] pr-[50px] flex gap-[12px] items-start cursor-pointer ">
 										<?php echo $question; ?>
 									</h3>
 									<div class="answer md:pr-[50px] pr-[30px] pl-[34px]" role="region">
-										<div class="font-body font-normal text-white text-[clamp(14px,1.7vw,16px)] leading-[clamp(22px,2.8vw,24px)] xl:pb-[32px] lg:pb-[24px] pb-[20px]">
+										<div class="font-body font-normal text-white text-[clamp(14px,1.7vw,16px)] leading-[clamp(22px,2.8vw,24px)] xl:pb-[24px] lg:pb-[24px] pb-[20px]">
 											<?php echo wp_kses_post( $answer ); ?>
 										</div>
 									</div>
