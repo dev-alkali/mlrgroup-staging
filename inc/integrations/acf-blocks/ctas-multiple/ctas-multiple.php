d3e7ec59285feca91547078c0bd7755daa3d1895
Dev Team <development@alkalidesigns.com>
2026-04-09 15:13:28 +0530

    Update ctas-multiple.php

--- inc/integrations/acf-blocks/ctas-multiple/ctas-multiple.php
+++ inc/integrations/acf-blocks/ctas-multiple/ctas-multiple.php
@@ -20,22 +20,19 @@ $title_row_2 = get_field('title_row_2');
 
 ?>
 
-<section id="<?php echo esc_attr($id); ?>" class="<?php echo esc_attr($className); ?> flex c-cta w-full bg-black text-white">
+<section id="<?php echo esc_attr($id); ?>" class="<?php echo esc_attr($className); ?> flex c-cta w-full bg-black">
   
   <div class="flex flex-col items-center w-full space-between gap-[40px] c-cta__wrap">
-    
-    <h2 class="font-heading">
+    <h2 class="c-cta__title font-heading text-white text-[clamp(36px,5vw,68px)] leading-[clamp(44px,5.5vw,78px)] tracking-[-0.02em] mb-0">
       <?php if($title_row_1): ?>
         <span class="font-bold"><?= wp_kses_post($title_row_1) ?></span>
       <?php endif; ?>
-
       <?php if($title_row_2): ?>
         <span class="font-light"><?= wp_kses_post($title_row_2) ?></span>
       <?php endif; ?>
     </h2>
-
-
     <?php if( have_rows('cta_items') ): ?>
+      <div class="flex flex-col gap-[22px] c-cta__buttons-wrap">
         <?php while( have_rows('cta_items') ): the_row(); ?>
           <?php $cta_item = get_sub_field('cta_item'); ?>
             <?php if( $cta_item ): 
@@ -43,12 +40,10 @@ $title_row_2 = get_field('title_row_2');
                 $title = $cta_item['title'];
                 $target = $cta_item['target'] ? $cta_item['target'] : '_self';
             ?>
-              <a class="btn-primary" href="<?php echo esc_url($url); ?>" target="<?php echo esc_attr($target); ?>"><?php echo esc_html($title); ?></a>
+              <a class="c-cta__button btn-primary" href="<?php echo esc_url($url); ?>" target="<?php echo esc_attr($target); ?>"><?php echo esc_html($title); ?></a>
             <?php endif; ?>
         <?php endwhile; ?>
+      </div>
     <?php endif; ?>
   </div>
 </section>
-
-
-
