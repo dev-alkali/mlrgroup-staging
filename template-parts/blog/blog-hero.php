55e52304902df66b31fee067856f61c1bd1cf4fe
devteam <development@alkalidesigns.com>
2026-04-10 15:03:58 +0530

    Blog hero

--- template-parts/blog/blog-hero.php
+++ template-parts/blog/blog-hero.php
@@ -9,7 +9,7 @@ if (have_rows('hero_blog', 'option')) : while (have_rows('hero_blog', 'option'))
   $bg_desktop_img = get_sub_field('blog_bg_image');
   $bg_mobile_image = get_sub_field('blog_bg_mobile_image');
 ?>
-    <section class="blog-hero w-full px-4 min-[600px]:px-10 min-[767px]:px-20 lg:px-[100px] bg-center bg-cover bg-no-repeat bg-[rgba(0,0,0,0.5)] bg-blend-overlay" style="--bg-desktop: url('<?php echo esc_url($bg_desktop_img); ?>');<?php if($bg_mobile_image): ?>--bg-mobile: url('<?php echo esc_url($bg_mobile_image); ?>');<?php endif; ?>">
+    <section class="blog-hero w-full px-4 md:px-10 bg-center bg-cover bg-no-repeat bg-[rgba(0,0,0,0.5)] bg-blend-overlay" style="--bg-desktop: url('<?php echo esc_url($bg_desktop_img); ?>');<?php if($bg_mobile_image): ?>--bg-mobile: url('<?php echo esc_url($bg_mobile_image); ?>');<?php endif; ?>">
       <div class="gap-10 w-full wrapper min-h-screen md:min-h-[670px] pt-[80px] md:pt-[118px] pb-[80px] md:pb-[60px] flex items-end !px-0">
         <div class="max-w-[870px]">
           <?php if ($title_row_1 || $title_row_2): ?>
@@ -18,7 +18,7 @@ if (have_rows('hero_blog', 'option')) : while (have_rows('hero_blog', 'option'))
               <span class="font-bold"><?php echo $title_row_1; ?></span>
             <?php endif; ?>
             <?php if ($title_row_2): ?>
-              <span class="font-light"><?php echo $title_row_2; ?></span>
+              <span class="font-bold"><?php echo $title_row_2; ?></span>
             <?php endif; ?>
           </h2>
           <?php endif; ?>
--- template-parts/case-study/case-study-hero.php
+++ template-parts/case-study/case-study-hero.php
@@ -9,7 +9,7 @@ if (have_rows('hero_case_study', 'option')) : while (have_rows('hero_case_study'
   $bg_desktop_img = get_sub_field('case_study_bg_image');
   $bg_mobile_image = get_sub_field('case_study_bg_mobile_image');
 ?>
-    <section class="case-study-hero w-full px-4 min-[600px]:px-10 min-[767px]:px-20 lg:px-[100px] bg-center bg-cover bg-no-repeat bg-[rgba(0,0,0,0.5)] bg-blend-overlay" style="--bg-desktop: url('<?php echo esc_url($bg_desktop_img); ?>');<?php if($bg_mobile_image): ?>--bg-mobile: url('<?php echo esc_url($bg_mobile_image); ?>');<?php endif; ?>">
+    <section class="case-study-hero w-full px-4 md:px-10 bg-center bg-cover bg-no-repeat bg-[rgba(0,0,0,0.5)] bg-blend-overlay" style="--bg-desktop: url('<?php echo esc_url($bg_desktop_img); ?>');<?php if($bg_mobile_image): ?>--bg-mobile: url('<?php echo esc_url($bg_mobile_image); ?>');<?php endif; ?>">
       <div class="gap-10 w-full wrapper min-h-screen md:min-h-[670px] pt-[80px] md:pt-[118px] pb-[80px] md:pb-[60px] flex items-end !px-0">
         <div class="max-w-[980px]">
           <?php if ($title_row_1 || $title_row_2): ?>
@@ -18,7 +18,7 @@ if (have_rows('hero_case_study', 'option')) : while (have_rows('hero_case_study'
               <span class="font-bold"><?php echo $title_row_1; ?></span>
             <?php endif; ?>
             <?php if ($title_row_2): ?>
-              <span class="font-light"><?php echo $title_row_2; ?></span>
+              <span class="font-bold"><?php echo $title_row_2; ?></span>
             <?php endif; ?>
           </h2>
           <?php endif; ?>
