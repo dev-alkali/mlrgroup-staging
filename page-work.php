d598d8fa54c4abd456200544798744cb300c7bfb
devteam <development@alkalidesigns.com>
2026-04-10 14:40:02 +0530

    Update page-work.php

--- page-work.php
+++ page-work.php
@@ -3,19 +3,18 @@
 
   <?php if (have_rows('hero_portfolio', 'option')) :  while (have_rows('hero_portfolio', 'option')) : the_row(); ?>
 
-      <section id="" class="w-full px-4 min-[600px]:px-10 min-[767px]:px-20 lg:px-[100px] bg-center bg-cover bg-no-repeat bg-[rgba(0,0,0,0.5)] bg-blend-overlay" style="  background-image: url('<?php echo esc_url(get_sub_field('bg_image')); ?>');
+      <section id="" class="w-full px-4 md:px-10 bg-center bg-cover bg-no-repeat bg-[rgba(0,0,0,0.5)] bg-blend-overlay" style="  background-image: url('<?php echo esc_url(get_sub_field('bg_image')); ?>');
     background-position: 50% 38%;
     background-size: cover;
     background-repeat: no-repeat;">
       <div class="gap-10 w-full wrapper min-h-screen md:min-h-[670px] pt-[80px] md:pt-[118px] pb-[80px] md:pb-[60px] flex items-end !px-0 ">
-        <div class="max-w-[970px]">
-          
+        <div class="max-w-[970px]">          
           <h2 class="text-[clamp(44px,6vw,70px)] leading-[clamp(56px,7vw,88px)] tracking-[-0.02em] text-white font-heading anim" data-delay="0.1" data-anim="up">            
             <?php if(get_sub_field('title_row_1')): ?>
                 <span class="font-bold"><?php echo get_sub_field('title_row_1'); ?></span>
             <?php endif; ?>
             <?php if(get_sub_field('title_row_2')): ?>
-                <span class="font-light"><?php echo get_sub_field('title_row_2'); ?> </span>
+                <span class="font-bold"><?php echo get_sub_field('title_row_2'); ?> </span>
             <?php endif; ?>
           </h2>
           
@@ -29,8 +28,8 @@
 <?php endwhile;
   endif; ?>
 
-  <section class="w-full flex flex-col items-center gap-10 md:gap-[60px] pt-10 md:pt-[60px] px-4 md:px-10">
-  <div class="flex flex-col md:flex-row items-start gap-[20px] md:gap-10 w-full max-w-[1920px] mx-auto max-[768px]:border-b border-[#CCCCCC] max-[768]:pb-[28px]">
+  <section class="w-full pt-10 md:pt-[60px] px-4 md:px-10">
+  <div class="flex flex-col md:flex-row items-start gap-[20px] md:gap-10 w-full wrapper mx-auto max-[768px]:border-b border-[#CCCCCC] max-[768]:pb-[28px]">
      <div class="md:border-b border-[#CCCCCC] md:pb-[20px] md:mb-[10px] w-full md:w-[220px] lg:w-[280px] xl:w-[380px]">
       <p class="text-[#525252] font-[Poppins] font-medium text-[16px] leading-[24px] md:text-[16px] md:leading-[24px]">Get inspired: Browse our portfolio, filter by category, add elements you like to your Inquiry List.</p>
      </div>
@@ -57,7 +56,7 @@
   <section
     class="w-full flex flex-col items-center gap-10 md:gap-[60px] pt-10 md:pt-[0px] pb-16 md:pb-[60px] bg-white px-4 md:px-10"
     aria-label="Portfolio gallery">
-    <div class="flex flex-col md:flex-row items-start gap-6 md:gap-10 w-full max-w-[1920px] mx-auto">
+    <div class="flex flex-col md:flex-row items-start gap-6 md:gap-10 w-full wrapper">
       <?php get_template_part('template-parts/portfolio/sidebar'); ?>
 
       <?php get_template_part('template-parts/portfolio/content-grid'); ?>
@@ -77,7 +76,7 @@
   ">
         <div class="flex flex-col items-center w-full h-full justify-end gap-[40px] min-[600px]:gap-[100px] min-[767px]:gap-[162px] pl-4 min-[600px]:pl-10 min-[767px]:pl-[140px] pr-4 min-[600px]:pr-10 min-[767px]:pr-20 pt-10 pb-[60px] min-[600px]:pb-[100px] [background:linear-gradient(222deg,rgba(0,0,0,0)_4.72%,rgba(0,0,0,1)_79.68%)]">
 
-          <div class="flex flex-col items-start gap-10 min-[600px]:gap-[60px] w-full max-w-[1920px]">
+          <div class="flex flex-col items-start gap-10 min-[600px]:gap-[60px] w-full wrapper">
             <div class="flex flex-col items-start gap-5 w-full">
               <h2 class="max-w-[622px] w-full text-[44px] tracking-[-2%] min-[767px]:text-[65px] min-[1024px]:text-[80px] leading-[50px] min-[767px]:leading-[65px] min-[1024px]:leading-[92px] text-white font-heading">
                 <span class="font-bold"> <?= wp_kses_post(get_sub_field('title_row_1')) ?> </span>
