3c87961248ac9f9e85ff14b83f7681b9db15fc86
devteam <development@alkalidesigns.com>
2026-04-22 19:27:59 +0530

    Update two-title-with-icon.php

--- inc/integrations/acf-blocks/two-title-with-icon/two-title-with-icon.php
+++ inc/integrations/acf-blocks/two-title-with-icon/two-title-with-icon.php
@@ -68,7 +68,7 @@ if ( $section_color == 'black' ){
 ?>
     <section id="<?php echo esc_attr($id); ?>" class="<?php echo esc_attr($className); ?> two-col-sec px-4 md:px-10 py-[60px] lg:py-[80px] xl:py-[120px] <?php echo $bg_color_class; ?><?php echo $pt_class; ?><?php echo $pb_class; ?>">
       
-      <div class="gap-[30px] md:gap-[0px] w-full wrapper flex flex-col md:flex-row two-column-wrapper">
+      <div class="gap-[30px] w-full wrapper flex flex-col md:flex-row two-column-wrapper">
 
         <!-- Left -->
         <?php if( have_rows('left_column') ): ?>
