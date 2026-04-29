9c33012b86f2ed32eb2ce622a0e1d9aa1899760c
devteam <development@alkalidesigns.com>
2026-04-29 15:06:04 +0530

    Update footer.php

--- footer.php
+++ footer.php
@@ -84,7 +84,7 @@
                   <div class="footer-title flex justify-between w-full items-center uppercase">
                     <div class="flex gap-1 items-center">                      
                       <a href="<?= esc_url($item->url) ?>"><?= esc_html($item->title) ?></a>
-                      <img class="w-4 h-4 mt[-3px]" src="<?= get_template_directory_uri() ?>/assets/imgs/Arrow-blue.svg" alt="">
+                      <img class="w-4 h-4 -mt-[3px]" src="<?= get_template_directory_uri() ?>/assets/imgs/Arrow-blue.svg" alt="">
                     </div>
                     <span class="dropdown-item-footer min-[600px]:hidden">
                       <img src="<?= get_template_directory_uri() ?>/assets/imgs/Caret-down.svg" alt="">
@@ -113,7 +113,7 @@
               <div class="footer-title flex justify-between w-full items-center uppercase">
                 <div class="flex gap-1 items-center">
                   <a href="<?= home_url(); ?>/contact-us/">Contact</a>
-                  <img class="w-4 h-4 mt[-3px]" src="/wp-content/themes/Mlrgroup/assets/imgs/Arrow-blue.svg" alt="">
+                  <img class="w-4 h-4 -mt-[3px]" src="/wp-content/themes/Mlrgroup/assets/imgs/Arrow-blue.svg" alt="">
                 </div>
                 <span class="dropdown-item-footer min-[600px]:hidden">
                   <img src="/wp-content/themes/Mlrgroup/assets/imgs/Caret-down.svg" alt="">
