7342a1b1c79571eb946963357adee37d04caded9
devteam <development@alkalidesigns.com>
2026-05-07 11:03:51 +0530

    Update two-title-with-icon.php

--- inc/integrations/acf-blocks/two-title-with-icon/two-title-with-icon.php
+++ inc/integrations/acf-blocks/two-title-with-icon/two-title-with-icon.php
@@ -104,9 +104,12 @@ if ( $section_color == 'black' ){
                   $content = $left_list['i_content'];
                   ?>
                   <div class="flex flex-row gap-[15px] w-full">
-                    <div class="flex w-[24px] h-[24px] relative">
-                      <img src="<?php echo get_template_directory_uri() ?>/assets/imgs/list-icon.svg" alt="" class="w-full">
+                    <div class="flex w-[15px] h-[15px] relative arrow-line mt-[4px] ml-[10px]">
+                      <img src="<?php echo get_template_directory_uri() ?>/assets/imgs/red-arrow-feature-sec.svg" alt="" class="w-full">
                     </div>
+                    <!-- <div class="flex w-[24px] h-[24px] relative">
+                      <img src="<?php //echo get_template_directory_uri() ?>/assets/imgs/list-icon.svg" alt="" class="w-full">
+                    </div> -->
                     <div class="flex flex-col flex-1">
                       <?php if($heading): ?>
                         <h3 class="text-[clamp(18px,2.6vw,20px)] leading-[clamp(22px,3.2vw,24px)] tracking-[-2%] text-[#262626] font-heading font-bold mb-[10px]"><?php echo $heading; ?></h3>
@@ -159,9 +162,12 @@ if ( $section_color == 'black' ){
                   $content2 = $right_list['i_content'];
                   ?>
                   <div class="flex flex-row gap-[15px] w-full">
-                    <div class="flex w-[24px] h-[24px] relative">
-                      <img src="<?php echo get_template_directory_uri() ?>/assets/imgs/list-icon.svg" alt="" class="w-full">
+                    <div class="flex w-[15px] h-[15px] relative arrow-line mt-[4px] ml-[10px]">
+                      <img src="<?php echo get_template_directory_uri() ?>/assets/imgs/red-arrow-feature-sec.svg" alt="" class="w-full">
                     </div>
+                    <!-- <div class="flex w-[24px] h-[24px] relative">
+                      <img src="<?php //echo get_template_directory_uri() ?>/assets/imgs/list-icon.svg" alt="" class="w-full">
+                    </div> -->
                     <div class="flex flex-col flex-1">
                       <?php if($heading2): ?>
                         <h3 class="text-[clamp(18px,2.6vw,20px)] leading-[clamp(22px,3.2vw,24px)] tracking-[-2%] text-[#262626] font-heading font-bold mb-[10px]"><?php echo $heading2; ?></h3>
