3192e6f4e4b34e6e27a3287f7f3f62f91dc51bc7
devteam <development@alkalidesigns.com>
2026-05-07 10:58:29 +0530

    Update map.php

--- inc/integrations/acf-blocks/map/map.php
+++ inc/integrations/acf-blocks/map/map.php
@@ -3743,13 +3743,13 @@ if ( ! empty( $section_remove_bottom_padding ) ) {
   <div class="max-w-[1440px] mr-auto ml-auto">
     <ul class="list-none border border-[#F5F5F5] max-w-[260px] p-5 rounded-[15px]">
       <li class="flex gap-[15px] items-center mb-[5px] text-[#525252] text-[14px] leading-snug">
-        <span class="text-right"><span style="width: 6px;height: 6px;border-radius: 50%;background:#000000;display: inline-block;"></span></span> <span>MRL Offices</span>
+        <span class="text-right w-[10px]"><span style="width: 6px;height: 6px;border-radius: 50%;background:#000000;display: inline-block;"></span></span> <span>MRL Offices</span>
       </li>
       <li class="flex gap-[15px] items-center mb-[5px] text-[#525252] text-[14px] leading-snug">
-        <span class="text-right"><span style="width: 10px;height: 10px;border-radius: 50%;background:#FD4338;display: inline-block;"></span></span> <span>Remote Workforce</span>
+        <span class="text-right w-[10px]"><span style="width: 10px;height: 10px;border-radius: 50%;background:#FD4338;display: inline-block;"></span></span> <span>Remote Workforce</span>
       </li>
       <li class="flex gap-[15px] items-center mb-[0px] text-[#525252] text-[14px]">
-        <span class="text-right"><span style="width: 10px;height: 10px;border-radius: 50%;background:#4772F2;display: inline-block;"></span></span> <span>Supplier Network</span>
+        <span class="text-right w-[10px]"><span style="width: 10px;height: 10px;border-radius: 50%;background:#4772F2;display: inline-block;"></span></span> <span>Supplier Network</span>
       </li>
     </ul>
   </div>
