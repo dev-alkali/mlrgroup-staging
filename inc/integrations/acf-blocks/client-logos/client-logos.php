18e25c846c33b9e947ca56c8d320305179b27c0d
devteam <development@alkalidesigns.com>
2026-05-20 11:38:51 +0530

    Update client-logos.php

--- inc/integrations/acf-blocks/client-logos/client-logos.php
+++ inc/integrations/acf-blocks/client-logos/client-logos.php
@@ -117,7 +117,7 @@ if (!empty($block['className'])) {
                 <img
                   src="<?php echo esc_url($logo['url']); ?>"
                   alt="<?php echo esc_attr($logo['alt']); ?>"
-                  class="w-[100%] max-w-[120px] md:w-[140px] lg:w-[130px] xl:w-[108px] 2xl:w-[135px] xl:max-h-[100%] object-contain h-auto"
+                  class="w-[100%] w-[120px] md:w-[140px] lg:w-[130px] xl:w-[108px] 2xl:w-[135px] xl:max-h-[100%] object-contain h-auto"
                 />
               <?php endif; ?>
             </div>
