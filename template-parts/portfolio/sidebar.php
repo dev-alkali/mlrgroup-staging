0866d91a608cff1f31d7824f1047b4c04133dfb6
devteam <development@alkalidesigns.com>
2026-06-09 11:29:18 +0530

    Update sidebar.php

--- template-parts/portfolio/sidebar.php
+++ template-parts/portfolio/sidebar.php
@@ -66,9 +66,9 @@ $filter_groups = get_field('portfolio_filter_groups', 'option');
                 <div class="flex items-start justify-between space-y-[28px]">
 
                   <?php if ($parent_text) : ?>
-                    <span class="<?php echo esc_attr($label_class); ?> <?php echo $has_children ? 'cursor-pointer' : ''; ?>" <?php echo $has_children ? 'data-toggle' : ''; ?>>
+                    <a href="<?php echo esc_url($parent_link); ?>" class="<?php echo esc_attr($label_class); ?> <?php echo $has_children ? 'cursor-pointer' : ''; ?>" <?php echo $has_children ? 'data-toggle' : ''; ?>>
                       <?php echo esc_html($parent_text); ?>
-                    </span>
+                    </a>
                   <?php else :
                     if (is_wp_error($parent_link)) continue;
                     if (!$has_children) {
