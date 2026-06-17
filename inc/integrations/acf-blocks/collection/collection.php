c102e25adc2a186affa14881315fe297c3a18a35
devteam <development@alkalidesigns.com>
2026-06-17 14:32:25 +0530

    Update collection.php

--- inc/integrations/acf-blocks/collection/collection.php
+++ inc/integrations/acf-blocks/collection/collection.php
@@ -107,7 +107,7 @@ if (!empty($block['className'])) {
                                         background-repeat: no-repeat;
                                     "' : ""; ?>>
                                 <a href="<?= esc_url($link_url) ?>" target="<?= esc_attr($link_target) ?>" class=" <?= $bg_image == "" ? "color-bg-hover" : "collection-gradient-box" ?> flex flex-col  items-end justify-end w-full h-full px-6 py-10 relative">
-                                    <div class="flex flex-col relative z-20 items-start <?= esc_url(get_sub_field('icon')) !== "" ? "" : "pt-[88px]"; ?> gap-5 relative self-stretch w-full flex-[0_0_auto]">
+                                    <div class="flex flex-col relative z-20 items-start <?= esc_url(get_sub_field('icon')) !== "" ? "" : "pt-[88px]"; ?> gap-5 relative self-stretch w-full flex-[0_0_auto] mb-[40px]">
                                     <?php if (esc_url(get_sub_field('icon')) !== ""): ?>
                                         <img
                                         Crop
