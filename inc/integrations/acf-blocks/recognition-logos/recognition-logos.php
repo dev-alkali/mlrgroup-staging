478ad48246405244db06b40b35d0bc690bead0c5
devteam <development@alkalidesigns.com>
2026-06-24 12:45:28 +0530

    Update recognition-logos.php

--- inc/integrations/acf-blocks/recognition-logos/recognition-logos.php
+++ inc/integrations/acf-blocks/recognition-logos/recognition-logos.php
@@ -74,7 +74,7 @@ if (!empty($block['className'])) {
             $in_title = $item['in_title'];
             $in_sub_title = $item['in_sub_title'];
           ?>
-            <div class="logo-card p-6 md:p-7 flex items-center text-center justify-center aspect-square relative sm:w-[calc(33.33%-6px)] w-[calc(50%-6px)]">
+            <div class="logo-card p-6 md:p-7 flex items-center text-center justify-center relative sm:w-[calc(33.33%-6px)] w-[calc(50%-6px)]">
               <?php if ($logo) : ?>
                 <figure><img
                   src="<?php echo esc_url($logo['url']); ?>"
