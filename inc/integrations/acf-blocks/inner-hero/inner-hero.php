39c825dab0e91c0ef720f2dd6a37a6ef2dce0525
development <development@alkalidesigns.com>
2026-07-13 11:22:54 +0530

    Update inner-hero.php

--- inc/integrations/acf-blocks/inner-hero/inner-hero.php
+++ inc/integrations/acf-blocks/inner-hero/inner-hero.php
@@ -36,14 +36,14 @@ if (have_rows('inner_hero')) :  while (have_rows('inner_hero')) : the_row();
       <div class="gap-10 w-full wrapper min-h-screen py-[80px] md:py-[118px] flex items-end">
         <div <?php echo $max_width_attr; ?>>
           <?php if($title_row_1 || $title_row_2): ?>
-          <h2 class="text-[clamp(44px,6vw,70px)] leading-[clamp(56px,7vw,88px)] tracking-[-0.02em] text-white font-heading anim" data-delay="0.1" data-anim="up">
+          <h1 class="text-[clamp(44px,6vw,70px)] leading-[clamp(56px,7vw,88px)] tracking-[-0.02em] text-white font-heading anim" data-delay="0.1" data-anim="up">
             <?php if($title_row_1): ?>
                 <span class="font-bold"><?= wp_kses_post($title_row_1) ?></span>
             <?php endif; ?>
             <?php if($title_row_2): ?>
                 <span class="font-bold"><?= wp_kses_post($title_row_2) ?></span>
             <?php endif; ?>
-          </h2>
+          </h1>
           <?php endif; ?>
           <?php if($description): ?>
             <p class="w-full text-[clamp(18px,3vw,20px)] leading-[28px] text-gray-50 font-body mt-[20px] anim" data-delay="1.2" data-anim="up" data-start="top 100%"><?= wp_kses_post($description) ?></p>
@@ -69,14 +69,14 @@ if (have_rows('inner_hero')) :  while (have_rows('inner_hero')) : the_row();
       <div class="gap-10 w-full wrapper min-h-screen md:min-h-[600px] pt-[80px] md:pt-[118px] pb-[80px] md:pb-[60px] flex items-end !px-0 ">
         <div <?php echo $max_width_attr; ?>>
           <?php if($title_row_1 || $title_row_2): ?>
-          <h2 class="text-[clamp(44px,6vw,70px)] leading-[clamp(56px,7vw,88px)] tracking-[-0.02em] text-white font-heading anim" data-delay="0.1" data-anim="up">          
+          <h1 class="text-[clamp(44px,6vw,70px)] leading-[clamp(56px,7vw,88px)] tracking-[-0.02em] text-white font-heading anim" data-delay="0.1" data-anim="up">          
             <?php if($title_row_1): ?>
                 <span class="font-bold"><?= wp_kses_post($title_row_1) ?></span>
             <?php endif; ?>
             <?php if($title_row_2): ?>
                 <span class="font-bold"><?= wp_kses_post($title_row_2) ?></span>
             <?php endif; ?>
-          </h2>
+          </h1>
           <?php endif; ?>
           <?php if($description): ?>
             <p class="w-full text-[clamp(18px,3vw,20px)] leading-[28px] text-gray-50 font-body mt-[20px] anim" data-delay="1.2" data-anim="up" data-start="top 100%"><?= wp_kses_post($description) ?></p>
