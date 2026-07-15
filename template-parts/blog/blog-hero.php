91092eb6dd0c2ca9cfb4801a85f0e4f625a89aaa
devteam <development@alkalidesigns.com>
2026-07-15 19:38:38 +0530

    CODE Update

--- template-parts/blog/blog-hero.php
+++ template-parts/blog/blog-hero.php
@@ -10,7 +10,7 @@ if (have_rows('hero_blog', 'option')) : while (have_rows('hero_blog', 'option'))
   $bg_mobile_image = get_sub_field('blog_bg_mobile_image');
 ?>
     <section class="blog-hero w-full px-4 md:px-10 bg-center bg-cover bg-no-repeat bg-[rgba(0,0,0,0.5)] bg-blend-overlay" style="--bg-desktop: url('<?php echo esc_url($bg_desktop_img); ?>');<?php if($bg_mobile_image): ?>--bg-mobile: url('<?php echo esc_url($bg_mobile_image); ?>');<?php endif; ?>">
-      <div class="gap-10 w-full wrapper min-h-screen md:min-h-[670px] pt-[80px] md:pt-[118px] pb-[80px] md:pb-[60px] flex items-end !px-0">
+      <div class="gap-10 w-full wrapper h-[calc(100vh-210px)] md:min-h-[670px] pt-[80px] md:pt-[118px] pb-[80px] md:pb-[60px] flex items-end !px-0">
         <div class="max-w-[870px]">
           <?php if ($title_row_1 || $title_row_2): ?>
           <h1 class="text-[clamp(44px,6vw,70px)] leading-[clamp(56px,7vw,88px)] tracking-[-0.02em] text-white font-heading anim" data-delay="0.1" data-anim="up">
